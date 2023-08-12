8188f84cba421a981bf80e46f606e67e75a2c49a
Yamini <95957635+Yamiinii@users.noreply.github.com>
2023-08-12 11:48:32 +0530

    Time: 9 ms (23.23%), Space: 7.6 MB (41.36%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
-    bool revStr(string& sol, int i){
-        int len=sol.length();
-        
-        if(i>=len/2) return true;
-        else if (sol[i]!=sol[len-i-1]) return false;
-        return revStr(sol, i+1);
+    bool valid(string &s,  int i) {
+        int n = s.length();
+        if (i >= n / 2)
+            return true;
+       else if (s[i] != s[n - i - 1])
+            return false;
+        return valid(s, i + 1);
     }
-
+    
     bool isPalindrome(string s) {
-        string sol;
+      string sol;
 
-        for(int i=0;i<s.length();i++){
-            if(s[i]>='A'&&s[i]<='Z') sol.push_back(tolower(s[i]));
-            else if(s[i]>='a'&&s[i]<='z'||s[i]>='0'&&s[i]<='9') sol.push_back(s[i]);
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] >= 'A' && s[i] <= 'Z')
+                sol.push_back(tolower(s[i]));
+            else if (s[i] >= 'a' && s[i] <= 'z' || s[i] >= '0' && s[i] <= '9')
+                sol.push_back(s[i]);
         }
-        return revStr(sol, 0);
+        
+        return valid(sol, 0);
+
     }
 };
