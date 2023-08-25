97209aee572f9a64cd39d38d7e997a63941e59cc
Yamini <95957635+Yamiinii@users.noreply.github.com>
2023-08-25 16:53:10 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (94.59%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -11,40 +11,57 @@
  */
 class Solution {
 public:
-    vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> v;
-        if(root==nullptr)
-            return v;
-        stack<TreeNode*> st;
-        TreeNode* curr = root;
-        TreeNode* temp = nullptr; // Define temp
+    void reverse(TreeNode* start, TreeNode* end) {
+        if (start == end)
+            return;
+        TreeNode* prev = start;
+        TreeNode* curr = start->right;
+        TreeNode* next;
+        while (prev != end) {
+            next = curr->right;
+            curr->right = prev;
+            prev = curr;
+            curr = next;
+        }
+    }
 
-        while(curr!=NULL || !st.empty())
-        {
-            if(curr!=NULL)
-            {
-                st.push(curr);
-                curr=curr->left;
-            }
-            else
-            {
-                temp=st.top()->right;
-                if(temp==nullptr)
-                {
-                    temp=st.top();
-                    st.pop();
-                    v.push_back(temp->val); // Push temp->val
-                    while(!st.empty() && temp==st.top()->right)
-                    {
-                        temp=st.top();
-                        st.pop();
-                        v.push_back(temp->val);
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> out;
+        if (root == nullptr)
+            return out;
+
+        TreeNode* dummy = new TreeNode(-1);
+        dummy->left = root;
+        root = dummy;
+        TreeNode* pre = nullptr;
+
+        while (root) {
+            if (root->left) {
+                pre = root->left;
+                while (pre->right && pre->right != root)
+                    pre = pre->right;
+
+                if (!pre->right) {
+                    pre->right = root;
+                    root = root->left;
+                } else {
+                    TreeNode* node = pre;
+                    reverse(root->left, pre);
+                    while (node != root->left) {
+                        out.push_back(node->val);
+                        node = node->right;
                     }
+                    out.push_back(node->val); // Print again since we are stopping at node = root->left
+                    reverse(pre, root->left);
+                    pre->right = nullptr;
+                    root = root->right;
                 }
-                else
-                    curr=temp;
+            } else {
+                root = root->right;
             }
         }
-        return v; // Return the vector
+
+        delete dummy;
+        return out;
     }
 };
