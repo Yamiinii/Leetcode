3fedc69c36f7393cbaf68b774f7a4bf1bb58bebe
Yamini <95957635+Yamiinii@users.noreply.github.com>
2023-08-25 11:52:22 +0530

    Time: 62 ms (66.98%), Space: 34.3 MB (29.54%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,43 +1,43 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int rows = mat.size();
-        int cols = mat[0].size();
-        
-        vector<vector<int>> dist(rows, vector<int>(cols, INT_MAX));
-        queue<pair<int, int>> q;
-        
-        // Initialize distances and add all 0 cells to the queue
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < cols; ++j) {
+        int r = mat.size();
+        int c = mat[0].size();
+        vector<vector<int>> ans(r, vector<int>(c, 0));
+        vector<vector<int>> visited(r, vector<int>(c, 0));
+        queue<pair<pair<int, int>, int>> q;
+
+        int dx[4] = {-1, 1, 0, 0};
+        int dy[4] = {0, 0, -1, 1};
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
                 if (mat[i][j] == 0) {
-                    dist[i][j] = 0;
-                    q.push({i, j});
+                    visited[i][j] = 1;
+                    q.push({{i, j}, 0});
                 }
             }
         }
-        
-        int x[4] = {1, -1, 0, 0};
-        int y[4] = {0, 0, 1, -1};
-        
+
         while (!q.empty()) {
-            int i = q.front().first;
-            int j = q.front().second;
+            int i = q.front().first.first;
+            int j = q.front().first.second;
+            int dist = q.front().second;
             q.pop();
-            
-            for (int k = 0; k < 4; ++k) {
-                int nx = i + x[k];
-                int ny = j + y[k];
-                
-                if (nx >= 0 && nx < rows && ny >= 0 && ny < cols) {
-                    if (dist[nx][ny] > dist[i][j] + 1) {
-                        dist[nx][ny] = dist[i][j] + 1;
-                        q.push({nx, ny});
-                    }
+
+            ans[i][j] = dist;
+
+            for (int k = 0; k < 4; k++) {
+                int ni = i + dx[k];
+                int nj = j + dy[k];
+
+                if (ni >= 0 && ni < r && nj >= 0 && nj < c && visited[ni][nj] == 0) {
+                    visited[ni][nj] = 1;
+                    q.push({{ni, nj}, dist + 1});
                 }
             }
         }
-        
-        return dist;
+
+        return ans;
     }
 };
