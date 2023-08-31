e0d2f4a606129266587a8f07f5b54ddcaf019096
Yamini <95957635+Yamiinii@users.noreply.github.com>
2023-08-31 14:36:51 +0530

    Time: 26 ms (69.37%), Space: 14.6 MB (55.15%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -4,42 +4,19 @@
 
 class Solution {
 public:
-    struct Interval {
-        int start;
-        int end;
-        Interval(int s, int e) : start(s), end(e) {}
-    };
-
-    static bool compareIntervals(const Interval& a, const Interval& b) {
-        return a.start < b.start;
-    }
-
     int minTaps(int n, std::vector<int>& ranges) {
-        std::vector<Interval> intervals;
-        for (int i = 0; i <= n; i++) {
-            int start = std::max(0, i - ranges[i]);
-            int end = std::min(n, i + ranges[i]);
-            intervals.push_back(Interval(start, end));
-        }
-        std::sort(intervals.begin(), intervals.end(), compareIntervals);
+        std::vector<int> dp(n + 1, n + 2); // Initialize with a value greater than n
+        dp[0] = 0;
 
-        int i = 0;
-        int currEnd = 0;
-        int nextEnd = 0;
-        int taps = 0;
+        for (int i = 0; i <= n; ++i) {
+            int left = std::max(0, i - ranges[i]);
+            int right = std::min(n, i + ranges[i]);
 
-        while (i < intervals.size() && currEnd < n) {
-            while (i < intervals.size() && intervals[i].start <= currEnd) {
-                nextEnd = std::max(nextEnd, intervals[i].end);
-                i++;
+            for (int j = left; j <= right; ++j) {
+                dp[j] = std::min(dp[j], dp[left] + 1);
             }
-            if (nextEnd == currEnd) {
-                return -1; // No progress can be made
-            }
-            currEnd = nextEnd;
-            taps++;
         }
 
-        return currEnd >= n ? taps : -1;
+        return dp[n] <= n ? dp[n] : -1;
     }
 };
