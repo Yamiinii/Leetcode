e2b482d9c57de2ffc43a17c2289b36c39af464d8
Yamini <95957635+Yamiinii@users.noreply.github.com>
2023-09-12 17:49:41 +0530

    Time: 206 ms (90.91%), Space: 484.4 MB (5.46%) - LeetHub

--- 2301-match-substring-after-replacement/2301-match-substring-after-replacement.cpp
+++ 2301-match-substring-after-replacement/2301-match-substring-after-replacement.cpp
@@ -1,36 +1,42 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
 class Solution {
 public:
-    unordered_map<char, unordered_map<char, char>> isMappingExist;
-    
-    bool check(string& s, int pos, string& sub) {
-        // Return if the substring cannot exist
-        if (pos + sub.size() > s.size()) {
+    bool check(string s, string sub, int pos, bool isMappingExist[256][256]) {
+        if (pos + sub.size() > s.size())
             return false;
-        }
-        
-        int index = 0;
+
+        int idx = 0;
         for (int i = pos; i < pos + sub.size(); i++) {
-            if (sub[index] == s[i] || isMappingExist[sub[index]][s[i]]) {
-                index++;
+            char sChar = s[i];
+            char subChar = sub[idx];
+
+            // Check if the characters match or a valid mapping exists
+            if (sChar == subChar || isMappingExist[subChar][sChar]) {
+                idx++;
             } else {
                 return false;
             }
         }
-        
         return true;
     }
-    
-    bool matchReplacement(string s, string sub, vector<vector<char>>& mappings) {
-        for (vector<char> mapping : mappings) {
-            isMappingExist[mapping[0]][mapping[1]] = 1;
+
+    bool matchReplacement(string s, string sub,vector<vector<char>>& mappings) {
+        bool isMappingExist[256][256] = {false}; // Initialize with ASCII character range
+        for (int i = 0; i < mappings.size(); i++) {
+            char subChar = mappings[i][0];
+            char sChar = mappings[i][1];
+            isMappingExist[subChar][sChar] = true;
         }
-        
         for (int i = 0; i < s.size(); i++) {
-            if (check(s, i, sub)) {
+            if (check(s, sub, i, isMappingExist)) {
                 return true;
             }
         }
-        
         return false;
     }
 };
+
