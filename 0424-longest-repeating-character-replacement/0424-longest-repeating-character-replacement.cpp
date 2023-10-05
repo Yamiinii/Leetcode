04da4a0b04c2a3b7e043dca59ebf88d3b1b7368b
Yamini <95957635+Yamiinii@users.noreply.github.com>
2023-10-05 15:08:44 +0530

    Time: 15 ms (26.59%), Space: 7.3 MB (23.57%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int head = 0, ans = 0;
-        int n = s.size();
-        vector<int> charCount(26, 0); // Store counts of each character
-        int maxCount = 0; // Maximum character count in the current window
-
-        for (int tail = 0; head < n; head++) {
-            charCount[s[head] - 'A']++;
-            maxCount = max(maxCount, charCount[s[head] - 'A']);
-
-            // Calculate the number of characters to replace
-            int replaceCount = (head - tail + 1) - maxCount;
-
-            // If the number of characters to replace is greater than k, move the tail
-            if (replaceCount > k) {
-                charCount[s[tail] - 'A']--;
-                tail++;
-            }
-
-            // Update the maximum length
-            ans = max(ans, head - tail + 1);
-        }
-
-        return ans;
+        
+        int i = 0, ans = 0;
+        int N=s.size();
+        int maxFreq=0;
+        map<int, int> freq;
+for (int j=0; j < N; ++j) {
+    freq[s[j]]++;
+    maxFreq=max(maxFreq,freq[s[j]]);
+    while(j-i+1-maxFreq>k)
+    {
+        freq[s[i++]]--;
+    }
+    ans = max(ans, j - i + 1); // the window [i, j] is the maximum window we've found thus far
+}
+return ans;
     }
 };
