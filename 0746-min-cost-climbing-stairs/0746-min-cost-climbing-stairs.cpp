0231fdf1cab837899c965e30ba7f586c92e63c04
Yamini <95957635+Yamiinii@users.noreply.github.com>
2023-10-19 15:09:30 +0530

    Time: 6 ms (36.89%), Space: 14.1 MB (24.46%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -2,14 +2,18 @@ class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        int dp = 0, dp1 = 0, dp2 = 0;
-        for (int i = 2; i <= n; i++) {
-            int oneStep = dp1 + cost[i - 1];
-            int twoStep = dp2 + cost[i - 2];
-            dp = min(oneStep, twoStep);
-            dp2 = dp1;
-            dp1 = dp;
+        vector<int> dp(n + 1, 0);
+        
+        // Initialize the DP array with the cost of the first two steps.
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        
+        for (int i = 2; i < n; i++) {
+            // Calculate the minimum cost to reach the current step.
+            dp[i] = cost[i] + min(dp[i - 1], dp[i - 2]);
         }
-        return dp1;
+        
+        // The final answer is the minimum cost to reach the last step or the second-to-last step.
+        return min(dp[n - 1], dp[n - 2]);
     }
 };
