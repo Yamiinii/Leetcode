23dfba3af24cb9bffd7144d9096cf728fb5b1e54
Yamini <95957635+Yamiinii@users.noreply.github.com>
2023-10-19 14:08:37 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (41.56%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,10 +1,14 @@
 class Solution {
+    int dp[31];
 public:
     int fib(int n) {
+        
         if(n<=0)
             return 0;
         else if(n==1)
             return 1;
-        return fib(n-1)+fib(n-2);
+        if(dp[n]!=0)
+            return dp[n];
+        return dp[n]=fib(n-1)+fib(n-2);
     }
 };
