3a2e379a752cd2ed6bf49cda34fe34d9bbeac70f
Yamini <95957635+Yamiinii@users.noreply.github.com>
2023-10-29 11:40:21 +0530

    Time: 503 ms (16.00%), Space: 13.5 MB (65.92%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,34 +1,38 @@
 class Solution {
 public:
-    bool canPartition(vector<int>& nums) {
-        int n = nums.size();
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += nums[i];
-        }
+   bool knapsack(vector<int>&arr, int sum)
+    {
+         int n = arr.size();
+    std::vector<std::vector<bool>> dp(n + 1, std::vector<bool>(sum + 1, false));
 
-        if (sum % 2 != 0) {
-            return false;
-        }
+    for (int i = 0; i <= n; i++) {
+        dp[i][0] = true;
+    }
 
-        sum = sum / 2;
-
-        vector<vector<bool>> dp(n + 1, vector<bool>(sum + 1, false));
-
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = true; // Subset with sum 0 is always possible
-        }
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= sum; j++) {
-                if (nums[i - 1] <= j) {
-                    dp[i][j] = dp[i - 1][j - nums[i - 1]] || dp[i - 1][j];
-                } else {
-                    dp[i][j] = dp[i - 1][j];
-                }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= sum; j++) {
+            if (arr[i - 1] <= j) {
+                dp[i][j] = dp[i - 1][j - arr[i - 1]] || dp[i - 1][j];
+            } else {
+                dp[i][j] = dp[i - 1][j];
             }
         }
+    }
 
-        return dp[n][sum];
+    return dp[n][sum];
+    }
+    
+    bool canPartition(vector<int>& nums) {
+        int sum=0;
+        int n=nums.size();
+        for(int i=0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+        }
+        if(sum%2!=0)
+            return false;
+        int target=sum/2;
+
+        return knapsack(nums,target)?true:false;
     }
 };
