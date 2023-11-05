93d2b77c2bf677a7c6bde3f8fca9c0f9103ed755
Yamini <95957635+Yamiinii@users.noreply.github.com>
2023-11-05 16:50:49 +0530

    Time: 3 ms (52.20%), Space: 7.4 MB (83.86%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        int pow_set_size = pow(2, nums.size());
-        
-         for (int counter = 0; counter < pow_set_size; counter++) {
-             vector<int> v;
-        for (int j = 0; j < nums.size(); j++) {
-            // Check if jth bit in the counter is set
-            // If set then print jth element from set
-            if (counter & (1 << j))
-                v.push_back(nums[j]);
+    void recursion(vector<int>& nums, vector<vector<int>>& ans, int i, int n, vector<int>& v) {
+        if (i == n) {
+            ans.push_back(v);
+            return;
         }
-        ans.push_back(v);
+        
+        v.push_back(nums[i]);
+        recursion(nums, ans, i + 1, n, v);
+        v.pop_back();
+        recursion(nums, ans, i + 1, n, v);
     }
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+        vector<int> v;
+        recursion(nums, ans, 0, n, v);
         return ans;
     }
 };
