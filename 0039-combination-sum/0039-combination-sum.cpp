9f185aec1765fc817a08be88ac5d8e36115569aa
Yamini <95957635+Yamiinii@users.noreply.github.com>
2023-11-06 23:21:59 +0530

    Time: 0 ms (100.00%), Space: 11.2 MB (64.03%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
-    void backtracking(vector<int>& candidates, int target, int start, vector<int>& current, vector<vector<int>>& result) {
-        if (target == 0) {
+    void backtracking(vector<int>& a, int target, int i, vector<int>& current, vector<vector<int>>& result, int sum) {
+        if (sum == target) {
             result.push_back(current);
             return;
         }
 
-        for (int i = start; i < candidates.size(); i++) {
-            if (candidates[i] <= target) {
-                current.push_back(candidates[i]);
-                backtracking(candidates, target - candidates[i], i, current, result);
-                current.pop_back();
-            }
+        if (i == a.size() || sum > target) {
+            return;
         }
+
+        // Include the current number in the combination
+        current.push_back(a[i]);
+        backtracking(a, target, i, current, result, sum + a[i]);
+        current.pop_back();
+
+        // Skip the current number and move to the next one
+        backtracking(a, target, i + 1, current, result, sum);
     }
-    
+
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> result;
         vector<int> current;
-        backtracking(candidates, target, 0, current, result);
+        int sum = 0;
+        backtracking(candidates, target, 0, current, result, sum);
         return result;
     }
 };
