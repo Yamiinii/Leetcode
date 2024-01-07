677c574da2976c0b49780ba39ec5cea318d016d9
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-01-07 10:43:49 +0530

    Time: 72 ms (82.22%), Space: 29.7 MB (93.61%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
@@ -1,34 +1,45 @@
 class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n, 0));
-        unordered_map<long, vector<int>> map;
-
-        for (int i = 0; i < n; i++) {
-            long temp = nums[i];
-            if (!map.count(temp)) {
-                map[temp] = vector<int>();
+        int n=nums.size();
+        if(n<=2)
+            return 0;
+        vector<vector<int>> dp(n,vector<int> (n,0));
+        unordered_map<long,vector<int>> mpp;
+        
+        for(int i=0;i<n;i++)
+        {
+            long temp=nums[i];
+            if(!mpp.count(temp))
+            {
+                mpp[temp]=vector<int>();
             }
-            map[temp].push_back(i);
+            
+            mpp[temp].push_back(i);
         }
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                //a is the prev element before nums[i] and nums[j] so nums[i]+nums[i]-nums[j]
+                long a=2L*nums[i]-nums[j];
+                if(mpp.count(a))
+                {
+                    for(int k:mpp[a])
+                    {
 
-        int sum = 0;
-        for (int i = 1; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                long a = 2L * nums[i] - nums[j];
-                if (map.count(a)) {
-                    for (int k : map[a]) {
-                        if (k < i) {
-                            dp[i][j] += dp[k][i] + 1;
-                        } else {
+                        if(k<i)
+                            dp[i][j]+=dp[k][i]+1;
+                        else
                             break;
-                        }
                     }
                 }
-                sum += dp[i][j];
+                sum+=dp[i][j];
             }
         }
+        
         return sum;
+        
     }
 };
