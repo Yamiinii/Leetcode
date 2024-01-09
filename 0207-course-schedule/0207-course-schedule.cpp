bf900b23f3317bcfc1a78e5838a1a210197accd8
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-01-09 18:11:29 +0530

    Time: 19 ms (58.58%), Space: 14.6 MB (44.77%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,41 +1,30 @@
 class Solution {
 public:
-  
-    
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int, vector<int>> adjList;
-        unordered_map<int,int> indegree;
-        for (auto& pre : prerequisites) {
-            adjList[pre[0]].push_back(pre[1]);
-            indegree[pre[1]]++;
-            
+bool iscycle(vector<int> adj[], vector<int> &vis, int id) {
+    if (vis[id] == 1)
+        return true;
+    if (vis[id] == 0) {
+        vis[id] = 1;
+        for (auto edge : adj[id]) {
+            if (iscycle(adj, vis, edge))
+                return true;
         }
-         queue<int> q;
-        for(int i=0;i<numCourses;i++)
-        {
-            if(indegree[i]==0)
-                q.push(i);
-        }
-        
-        vector<int> courses;
-        
-        while (!q.empty()) {  // Corrected the condition here
-            int node=q.front();
-            q.pop();
-            courses.push_back(node);
-            for (auto it : adjList[node]) {
-               indegree[it]--;
-                if(indegree[it]==0)
-                    q.push(it);
-                
-                }
-            }
-        
-       if(courses.size()==numCourses)
-           return true;
-        else
-        return false;
-        
-        
     }
+    vis[id] = 2;
+    return false;
+}
+
+bool canFinish(int n, vector<vector<int>>& pre) {
+    vector<int> adj[n];
+    for (auto edge : pre)
+        adj[edge[1]].push_back(edge[0]);
+    vector<int> vis(n, 0);
+    
+    for (int i = 0; i < n; i++) {
+        if (iscycle(adj, vis, i))
+            return false;
+    }
+    return true;
+}
+
 };
