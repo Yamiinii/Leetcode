30ac01ea1bb4327995dd7158e2f55c75d1546c2b
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-01-21 17:03:22 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (9.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,20 +1,51 @@
 class Solution {
 public:
-    int solve(vector<int> &nums, int size, vector<int> &dp) {
-    if (size < 0)
-        return 0;
-    if (size == 0)
-        return nums[0];
-    if (dp[size] != -1)
-        return dp[size];
-    
-    int incl = solve(nums, size - 2, dp) + nums[size];
-    int excl = solve(nums, size - 1, dp);
-
-    return dp[size] = max(incl, excl);
-}
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size(), -1); // Initialize with -1 for uncomputed values
-    return solve(nums, nums.size() - 1, dp);
+         int n = nums.size();
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+
+        std::vector<int> dp(n, 0);
+        dp[0] = nums[0];
+        dp[1] = std::max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; ++i) {
+            dp[i] = std::max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+
+        return dp[n - 1];
+//         int n = nums.size();
+//         if (n == 0) {
+//             return 0;
+//         }
+
+//         vector<int> dp(n);
+//         dp[0] = nums[0];
+
+//         for (int i = 1; i < n; i++) {
+//             int steps = INT_MIN;
+//             for (int j = 2; j <= min(i, 2); j++) {
+//                 if (i - j >= 0) {
+//                     int step = dp[i - j] + nums[i];
+//                     steps = max(steps, step);
+//                 }
+//             }
+//             dp[i] = max(steps, dp[i - 1]);
+//         }
+
+//         return dp[n - 1];
+        // counter example-->nums = [2, 1, 1, 2]
+        // int n=nums.size();
+        // int sum1;
+        // for(int i=0;i<n;i=i+2)
+        // {
+        //     sum1+=nums[i];
+        // }
+        //     int sum2;
+        // for(int i=1;i<n;i=i+2)
+        // {
+        //     sum2+=nums[i];
+        // }
+        // return max(sum1,sum2);
     }
 };
