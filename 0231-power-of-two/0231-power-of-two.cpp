44e76e8c59b9147e47fa94d2a1645bec80009356
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-02-20 14:50:19 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (19.56%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
+    bool f(int n)
+    {
+        if(n==1)
+            return true;
+        if(n%2!=0)
+            return false;
+        return f(n/2);
+    }
+    
     bool isPowerOfTwo(int n) {
-           int cnt = 0;
-    while (n > 0) {
-        if ((n & 1) == 1) {
-            cnt++;
-        }
-        n = n >> 1; // keep dividing n by 2 using right
-                    // shift operator
-    }
- 
-    if (cnt == 1) { // if cnt = 1 only then it is power of 2
-        return true;
-    }
-    return false;
+        if(n<=0)
+            return false;
+        return f(n);
     }
 };
