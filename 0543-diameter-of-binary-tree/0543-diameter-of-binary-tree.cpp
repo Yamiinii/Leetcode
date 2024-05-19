ea97b722b34c6f304e9ec12c83a6f92acc61a1ae
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-05-19 20:43:14 +0530

    Time: 19 ms (25.74%), Space: 22.2 MB (45.02%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -9,24 +9,33 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-    int res;
-public:
-    int Solve(TreeNode* root) {
-         if (root == NULL)
-            return 0;
-     int l = Solve(root->left);
-        int r = Solve(root->right);
 
-        int temp = 1 + max(l, r);
-        int ans = max(temp, l + r + 1);
-        res = max(res, ans);
-        return temp;
+class Solution {
+public:
+    int height(TreeNode* root) {
+        if (root == nullptr)
+            return 0;
+        int l = height(root->left);
+        int r = height(root->right);
+        return max(l, r) + 1;
+    }
+    
+    int diameterOfBinaryTree(TreeNode* root) {
+        int diameter = 0;
+        heightAndDiameter(root, diameter);
+        return diameter;
     }
 
-    int diameterOfBinaryTree(TreeNode* root) {
-       
-        Solve(root);
-        return res-1;
+private:
+    int heightAndDiameter(TreeNode* root, int& diameter) {
+        if (root == nullptr)
+            return 0;
+        int leftHeight = heightAndDiameter(root->left, diameter);
+        int rightHeight = heightAndDiameter(root->right, diameter);
+        
+        // Update the diameter if the path through the root is larger
+        diameter = max(diameter, leftHeight + rightHeight);
+        
+        return max(leftHeight, rightHeight) + 1;
     }
 };
