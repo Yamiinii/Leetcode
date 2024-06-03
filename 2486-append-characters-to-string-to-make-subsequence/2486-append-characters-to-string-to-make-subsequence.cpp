0f8423c333b7605aac8ae842cf5a04d4f2a6fc50
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-06-03 20:12:37 +0530

    Time: 15 ms (87.56%), Space: 11.9 MB (83.42%) - LeetHub

--- 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.cpp
+++ 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    int appendCharacters(string s, string t) {
-        int n = s.size();
-        int m = t.size();
-        int i = 0;
-        int j = 0;
-
-        // Traverse through s and t to find the length of the longest subsequence of t in s
-        while (i < n && j < m) {
-            if (s[i] == t[j]) {
+    
+int appendCharacters(string s, string t) {
+       int n=s.size();
+        int m=t.size();
+        int i=0;
+        int j=0;
+        while(i<n && j<m)
+        {
+            if(s[i]==t[j])
+            {
                 j++;
             }
-            i++;
+                i++;
         }
-        
-        // The number of characters to be appended is the length of t minus the longest subsequence found
-        return m - j;
+        return m-j;
     }
 };
