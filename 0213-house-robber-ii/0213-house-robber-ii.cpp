128fce487ed4dfa24f8c249222b6d78ff274ca3d
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-06-24 22:27:18 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (29.07%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,35 +1,29 @@
-#include <vector>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
-    int solve(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 0) return 0;
+    int solve(vector<int>& nums)
+    {
+        vector<int> dp(nums.size(),0);
+        int n=nums.size();
+                if (n == 0) return 0;
         if (n == 1) return nums[0];
-        
-        vector<int> dp(n);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
-        
-        for (int i = 2; i < n; i++) {
-            dp[i] = max(nums[i] + ( dp[i - 2]), dp[i - 1]);
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+        for(int i=2;i<n;i++)
+        {
+            int take=nums[i]+dp[i-2];
+            int nottake=dp[i-1];
+            dp[i]=max(take,nottake);
         }
-        
-        return dp[n - 1];
+        return dp[n-1];
     }
-
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 1) return nums[0];
+int n=nums.size();
+        if(n==1)
+            return nums[0];
+ vector<int> arr1(nums.begin() + 1, nums.end());
         
-        // Scenario 1: Rob houses from the first house to the second-to-last house
-        vector<int> arr1(nums.begin(), nums.end() - 1);
-        
-        // Scenario 2: Rob houses from the second house to the last house
-        vector<int> arr2(nums.begin() + 1, nums.end());
-        
-        return max(solve(arr1), solve(arr2));
+        // Option 2: Exclude the last house
+        vector<int> arr2(nums.begin(), nums.end() - 1);
+        return max(solve(arr1),solve(arr2));
     }
 };
