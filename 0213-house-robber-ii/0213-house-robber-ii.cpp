65b39febacc79afd1c887bb4d78bd5b497641bf9
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-06-24 22:19:54 +0530

    Time: 4 ms (33.06%), Space: 9.4 MB (49.43%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -14,7 +14,7 @@ public:
         dp[1] = max(nums[0], nums[1]);
         
         for (int i = 2; i < n; i++) {
-            dp[i] = max(nums[i] + (i > 1 ? dp[i - 2] : 0), dp[i - 1]);
+            dp[i] = max(nums[i] + ( dp[i - 2]), dp[i - 1]);
         }
         
         return dp[n - 1];
