5904089f7101339a9ca66cc5261bc01f40e15b82
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-06-24 23:04:07 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (17.03%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
        int rec(int m, int n, int i, int j, std::vector<std::vector<int>>& dp) {
      
+        if(i>=m ||j>=n)
+            return 0;
+           
         if (i == m - 1 && j == n - 1) {
             return dp[i][j] = 1;
         }
@@ -10,18 +13,7 @@ public:
             return dp[i][j];
         }
         
-        int right = 0, down = 0;
-     
-        if (j + 1 < n) {
-            right = rec(m, n, i, j + 1, dp);
-        }
-        
-        if (i + 1 < m) {
-            down = rec(m, n, i + 1, j, dp);
-        }
-        
-     
-        return dp[i][j] = right + down;
+        return dp[i][j] = rec(m,n,i+1,j,dp)+rec(m,n,i,j+1,dp);
     }
     
     int uniquePaths(int m, int n) {
