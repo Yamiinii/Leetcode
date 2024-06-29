b3be3a6253bc8467f0e2130d08c667bd3bedfa13
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-06-29 19:23:29 +0530

    Time: 9 ms (58.25%), Space: 16.6 MB (26.31%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,45 +1,29 @@
 class Solution {
 public:
-    void markCol(vector<vector<int>>& matrix,int i,int m)
-    {
-        for(int row=0;row<m;row++)
-        {
-            if(matrix[row][i]!=0)
-                matrix[row][i]=-999999;
-        }
-    }
-    
-    void markRow(vector<vector<int>>& matrix,int i,int m)
-    {
-        for(int col=0;col<m;col++)
-        {
-            if(matrix[i][col]!=0)
-                matrix[i][col]=-999999;
-        }
-    }
-    
+    //////////////////////better
     void setZeroes(vector<vector<int>>& matrix) {
-        //brute
-        int n=matrix.size();
-        int m=matrix[0].size();
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(matrix[i][j]==0)
-                {
-                    markRow(matrix,i,m);
-                    markCol(matrix,j,n);
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        // Boolean arrays to track which rows and columns need to be zeroed
+        vector<bool> zeroRow(n, false);
+        vector<bool> zeroCol(m, false);
+        
+        // First pass: Mark rows and columns to be zeroed
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    zeroRow[i] = true;
+                    zeroCol[j] = true;
                 }
             }
         }
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(matrix[i][j]==-999999)
-                {
-                    matrix[i][j]=0;
+        
+        // Second pass: Zero out marked rows and columns
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (zeroRow[i] || zeroCol[j]) {
+                    matrix[i][j] = 0;
                 }
             }
         }
