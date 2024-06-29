65f4b9d2e3e7127150660df58f68de98769e7bac
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-06-29 21:52:16 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (7.15%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-
-    vector<int> genRow(int i)
-    {
-        vector<int> v;
-       long long res=1;
-        v.push_back(1);
-        for(int j=1;j<i;j++)
-        {
-            res=res*(i-j);
-            res=res/j;
-            v.push_back(res);
-        }
-        return v;
-
-    }
-
+    //for a specific position using (n-1)C(r-1)
+    //it is found using num=num*(num-1)
+    //num/i+1
+    //for a complete row we see the trend as val=prev*(row-col)/col
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> ans;
-        for(int i=1;i<=numRows;i++)
+        vector<int> v1;
+        v1.push_back(1);
+        ans.push_back(v1);
+        if(numRows==1)
+            return ans;
+        
+        for(int i=1;i<numRows;i++)
         {
-            vector<int> rowlist=genRow(i);
-            ans.push_back(rowlist);
+            vector<int> v;
+            v.push_back(1);
+            for(int j=1;j<i;j++)
+            {
+               v.push_back(ans[i-1][j-1]+ans[i-1][j]);   
+            }
+            v.push_back(1);
+            ans.push_back(v);
         }
         return ans;
     }
