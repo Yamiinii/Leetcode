7319508331d81bcfba16913f05542af65a354dde
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-06-30 16:40:30 +0530

    Time: 0 ms (100.00%), Space: 9.8 MB (72.40%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,55 +1,27 @@
 class Solution {
 public:
+    //we can count the values and then put
+    //for mid dont do anything
     void sortColors(vector<int>& nums) {
-     int n=nums.size();   
-     int start=0;
-     int mid=0;
-     int end=n-1; 
-    while(mid<=end)
-    {
-        if(nums[mid]==0)
+        int start=0;
+        int mid=0;
+        int end=nums.size()-1;
+        while (mid <= end)
         {
-            swap(nums[mid],nums[start]);
-            start++;
-            mid++;
+            if(nums[mid]==0)
+            {
+                swap(nums[start],nums[mid]);
+                mid++;
+                start++;
+            }
+            else if(nums[mid]==1)
+            {
+                mid++;
+            }
+       else { // nums[mid] == 2
+                swap(nums[mid], nums[end]);
+                end--;
+            }
         }
-        else if(nums[mid]==1)
-        {
-             mid++;
-        }
-        else
-        {
-            swap(nums[mid],nums[end]);
-            end--;
-        }
-    }
-
-
-
-// int zero = 0;
-// int one = 0;
-// int two = 0;
-
-// for (int i = 0; i < n; i++) {
-//     if (nums[i] == 0)
-//         zero++;
-//     else if (nums[i] == 1)
-//         one++;
-//     else
-//         two++;
-// }
-
-// for (int i = 0; i < zero; i++) {
-//     nums[i] = 0;
-// }
-
-// for (int i = zero; i < zero + one; i++) {
-//     nums[i] = 1;
-// }
-
-// for (int i = zero + one; i < zero + one + two; i++) {
-//     nums[i] = 2;
-// }
-
     }
 };
