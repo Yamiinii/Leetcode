a80606fed757858e8fd2c6fe9e43ce551b890a27
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-07-02 22:53:51 +0530

    Time: 20 ms (74.74%), Space: 25.7 MB (54.21%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,35 +1,33 @@
-#include <string>
-#include <vector>
-#include <algorithm>
-
 class Solution {
 public:
-    
-
-    int longestCommonSubsequence(const std::string& s1, const std::string& s2) {
-int n = s1.size();
-    int m = s2.size();
-
-    vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1)); // Create a DP table
-
-    // Initialize the base cases
-    for (int i = 0; i <= n; i++) {
-        dp[i][0] = 0;
-    }
-    for (int i = 0; i <= m; i++) {
-        dp[0][i] = 0;
-    }
-
-    // Fill in the DP table to calculate the length of LCS
-    for (int ind1 = 1; ind1 <= n; ind1++) {
-        for (int ind2 = 1; ind2 <= m; ind2++) {
-            if (s1[ind1 - 1] == s2[ind2 - 1])
-                dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1]; // Characters match, increment LCS length
-            else
-                dp[ind1][ind2] = max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]); // Characters don't match, consider the maximum from left or above
+    int longestCommonSubsequence(string text1, string text2) {
+        int n=text1.size();
+        int m=text2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        // for(int i=0;i<=n;i++)
+        // {
+        //     for(int j=0;j<=m;j++)
+        //     {
+        //         if(i==0 || j==0)
+        //         {
+        //             dp[i][j]=0;
+        //         }
+        //     }
+        // }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(text1[i-1]==text2[j-1])
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
         }
-    }
-
-    return dp[n][m];
+        return dp[n][m];
     }
 };
