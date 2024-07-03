3baa5a65ac44cd5bb597ab86536cb04c7d3ab125
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-07-03 23:29:06 +0530

    Time: 34 ms (38.93%), Space: 29.6 MB (65.93%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,38 +1,22 @@
 class Solution {
 public:
-    
-         int longestCommonSubsequence(const std::string& s1, const std::string& s2) {
-int n = s1.size();
-    int m = s2.size();
-
-    vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1)); // Create a DP table
-
-    // Initialize the base cases
-    for (int i = 0; i <= n; i++) {
-        dp[i][0] = 0;
-    }
-    for (int i = 0; i <= m; i++) {
-        dp[0][i] = 0;
-    }
-
-    // Fill in the DP table to calculate the length of LCS
-    for (int ind1 = 1; ind1 <= n; ind1++) {
-        for (int ind2 = 1; ind2 <= m; ind2++) {
-            if (s1[ind1 - 1] == s2[ind2 - 1])
-                dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1]; // Characters match, increment LCS length
-            else
-                dp[ind1][ind2] = max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]); // Characters don't match, consider the maximum from left or above
+    int minInsertions(string X) {
+         string Y = X; // Create a copy of X
+        reverse(Y.begin(), Y.end()); // Reverse Y
+        int m=X.size();
+        int n=Y.size();
+         vector<vector<int>> v(m+1,vector<int>(n+1,0));
+        
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(X[i-1]==Y[j-1])
+                v[i][j]=1+v[i-1][j-1];
+                else
+                v[i][j]=max(v[i-1][j],v[i][j-1]);
+            }
         }
-    }
-
-    return dp[n][m];
-    }
-    int longestPalindromeSubseq(string s) {
-        string s1=s;
-        reverse(s1.begin(),s1.end());
-        return longestCommonSubsequence(s,s1);
-    }
-    int minInsertions(string s) {
-        return s.size()-longestPalindromeSubseq(s);
+        return m-v[m][n];
     }
 };
