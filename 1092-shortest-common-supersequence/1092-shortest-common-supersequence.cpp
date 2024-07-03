a02793c014c6eec2a354ec02581f9f6f8605e2d4
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-07-03 22:15:20 +0530

    Time: 21 ms (67.45%), Space: 25.6 MB (69.50%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,51 +1,54 @@
 class Solution {
 public:
-    string shortestCommonSupersequence(string s1, string s2) {
-        int m=s1.size();
-        int n=s2.size();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-
-    for (int i = 1; i <= m; ++i) {
-        for (int j = 1; j <= n; ++j) {
-            if (s1[i - 1] == s2[j - 1]) {
-                dp[i][j] = 1 + dp[i - 1][j - 1];
-            } else {
-                dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+    string shortestCommonSupersequence(string X, string Y) {
+        int m=X.size();
+        int n=Y.size();
+         vector<vector<int>> v(m+1,vector<int>(n+1,0));
+        
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(X[i-1]==Y[j-1])
+                v[i][j]=1+v[i-1][j-1];
+                else
+                v[i][j]=max(v[i-1][j],v[i][j-1]);
             }
         }
+       string s="";
+while(m>0 && n>0)
+{
+    if(X[m-1]==Y[n-1])
+    {
+        s+=X[m-1];
+        m--;
+        n--;
     }
-
-    int i = m, j = n;
-    string lcs;  // To store the LCS.
-
-    while (i > 0 && j > 0) {
-        if (s1[i - 1] == s2[j - 1]) {
-            lcs.push_back(s1[i - 1]);
-            i--;
-            j--;
-        } else {
-            if (dp[i - 1][j] > dp[i][j - 1]) {
-                lcs.push_back(s1[i - 1]);
-                i--;
-            } else {
-                lcs.push_back(s2[j - 1]);
-                j--;
-            }
-        }
+    else if(v[m-1][n]>=v[m][n-1])
+    {
+        s+=X[m-1];
+        m--;
     }
-        while(i>0)
-        {
-            lcs.push_back(s1[i - 1]);
-            i--;
-        }
-        while (j>0)
-        {
-            lcs.push_back(s2[j - 1]);
-            j--;
-        }
+    else
+    {
+        s+=Y[n-1];
+        n--;
+    }
+}
+while(m>0)
+{
+    s+=X[m-1];
+    m--;
+}
+while(n>0)
+{
+    s+=Y[n-1];
+    n--;
+}
 
-    reverse(lcs.begin(), lcs.end());  // Reverse the LCS string.
+reverse(s.begin(),s.end());
+return s;
 
-    return lcs;
+        
     }
 };
