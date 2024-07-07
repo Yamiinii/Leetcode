f02b80770d1c27acfc5052a302d776deefb7d310
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-07-07 18:27:54 +0530

    Time: 151 ms (28.70%), Space: 39 MB (13.71%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.cpp
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -1,33 +1,36 @@
 class Solution {
-    unordered_map<string,bool> mpp;
 public:
+    unordered_map<string,bool> mpp;
     bool solve(string s1, string s2)
     {
-        if(s1.compare(s2)==0)
-            return true;
+        string key=s1+" "+s2;
         
-        if(s1.length()<=1)
-         
-            return false;
-       string key = s1 + " " + s2;
-	if (mpp.find(key) != mpp.end()) // if we did not found the and travesed upto end of the map 
-		return mpp[key];
-        bool flag=false;
+        if(s1==s2)
+            return mpp[key]=true;
+        //empty string not possible
         int n=s1.length();
-        for(int i=1;i<=s1.length()-1;i++)
+        if(n<=1)
+            return mpp[key]=false;
+        
+        if(mpp.find(key)!=mpp.end())
+            return mpp[key];
+        bool flag=false;
+        
+    for(int k=1;k<n;k++)
         {
-            if((solve(s1.substr(0,i),s2.substr(n-i,i)) && solve(s1.substr(i),s2.substr(0,n-i))) ||
-              (solve(s1.substr(0,i),s2.substr(0,i)) && solve(s1.substr(i),s2.substr(i))))
-               { flag=true;
-            break;}
+            if((solve(s1.substr(0,k),s2.substr(n-k,k)) && solve(s1.substr(k),s2.substr(0,n-k))) || 
+              (solve(s1.substr(0,k),s2.substr(0,k)) && solve(s1.substr(k),s2.substr(k))))
+            {
+                flag=true;
+                break;
+            }
         }
+        
         return mpp[key]=flag;
+        
     }
     
     bool isScramble(string s1, string s2) {
-        mpp.clear();
-        if(s1.length()!=s2.length())
-            return false;
         return solve(s1,s2);
     }
 };
