6e9ea5e92da2b8d1f14d5c57be6dc64dc8a935cc
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-07-10 23:55:33 +0530

    Time: 0 ms (100.00%), Space: 9.7 MB (70.46%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,19 +11,47 @@
  */
 class Solution {
 public:
-      void trav(TreeNode* root, vector<int>& v)
-    {
-        if (root == nullptr) {
-            return;
-        }
-          trav(root->left, v);
-        v.push_back(root->val);
+//       void trav(TreeNode* root, vector<int>& v)
+//     {
+//         if (root == nullptr) {
+//             return;
+//         }
+//           trav(root->left, v);
+//         v.push_back(root->val);
       
-        trav(root->right, v);
-    }
+//         trav(root->right, v);
+    //}
     vector<int> inorderTraversal(TreeNode* root) {
-         vector<int> result;
-        trav(root, result);
-        return result;
+        vector<int> inorder;
+        TreeNode* curr=root;
+        while(curr!=NULL)
+        {
+            if(curr->left==NULL)
+            {
+                inorder.push_back(curr->val);
+                curr=curr->right;
+            }
+            else
+            {
+                TreeNode* prev=curr->left;
+                while(prev->right && prev->right!=curr)
+                {
+                    prev=prev->right;
+                }
+                
+                if(prev->right==NULL)
+                {
+                    prev->right=curr;
+                    curr=curr->left;
+                }
+                else
+                {
+                    prev->right=NULL;
+                    inorder.push_back(curr->val);
+                    curr=curr->right;
+                }
+            }
+        }
+        return inorder;
     }
 };
