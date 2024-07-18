79ee6f2272da5205a9b1697143b941ebb861e5c5
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-07-18 22:28:07 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (11.18%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -1,14 +1,34 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
- 
-int cnt=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            for(int j=i+1;j<nums.size();j++)
-            {if(nums[i]==nums[j])
-                cnt++;}
+        //no space
+    int total_count = 0;
+        int same_count = 0;
+        int curr_num = -1;
+        std::sort(nums.begin(),nums.end());
+        
+        for(int i = 0 ; i < nums.size(); i++){
+            if(curr_num!=nums[i]){
+                curr_num = nums[i];
+                same_count = 0;
+            }else{
+                total_count += same_count + 1;
+                same_count += 1;
+            }
         }
-        return cnt;
+        return total_count; 
+
+        // best solution
+
+//         unordered_map<int,int>umap;
+//         for(auto num:nums){
+//             umap[num] += 1;
+//         }
+//         int total_count = 0;
+//         for(auto ans:umap){
+//             total_count += (ans.second*(ans.second-1))/2;
+//             //to choose from n identical items nc2 is the way i.e. n(n-1)/2
+//         }
+//         return total_count;
     }
 };
