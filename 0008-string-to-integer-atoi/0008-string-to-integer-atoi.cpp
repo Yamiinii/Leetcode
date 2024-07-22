a39f1d7e990442757ba7ea110d1fbe9bc07ccdd0
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-07-22 19:05:02 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (17.06%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
     int myAtoi(string s) {
-       bool positive = true; // Default to positive
-        int result = 0;
-
-        int i = 0;
-        while (i < s.length() && s[i] == ' ') {
+       int i=0;
+        int sign=1;
+        long ans=0;
+        while(i<s.length() && s[i]==' ')
+            i++;
+        if(s[i]=='-')
+        {
+            sign=-1;
             i++;
         }
-
-        if (i < s.length() && (s[i] == '+' || s[i] == '-')) {
-            positive = (s[i] == '+');
+        else if(s[i]=='+')
             i++;
-        }
-
-        while (i < s.length() && isdigit(s[i])) {
-            int digit = s[i] - '0';
-            
-            // Check for integer overflow
-            if (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > 7)) {
-                return (positive) ? INT_MAX : INT_MIN;
+        while(i<s.length())
+        {
+            if(s[i]>='0' && s[i]<='9')
+            {
+                ans=ans*10+(s[i]-'0');
+                if(ans>INT_MAX && sign==-1)
+                    return INT_MIN;
+                else if(ans>INT_MAX && sign==1)
+                    return INT_MAX;
+                i++;
             }
-
-            result = result * 10 + digit;
-            i++;
+            else
+                return ans*sign;
         }
-
-        return (positive) ? result : -result;
+        return (ans*sign);
     }
 };
