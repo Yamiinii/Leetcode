d13ccdcb9f6d58de8c00b896c39dd5c0203a4b64
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-07-22 18:53:54 +0530

    Time: 8 ms (77.07%), Space: 12 MB (40.34%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map<char, int> mpp;
-        int maxi = 0;
-        int start = 0;
-        int n = s.size();
-
-        for (int end = 0; end < n; end++) {
-            char currentChar = s[end];
-
-            if (mpp.find(currentChar) != mpp.end() && mpp[currentChar] >= start) {
-                // If current character is already in the map and its index is within the current window
-                start = mpp[currentChar] + 1;
+       int n=s.size();
+        unordered_map<char,int>mpp;
+        int left=0;
+        int maxLength=0;
+        for(int right=0;right<n;right++)
+        {
+            if (mpp.count(s[right]) == 0 || mpp[s[right]] < left) {
+                mpp[s[right]]=right;
+                maxLength = max(maxLength, right - left + 1);
             }
-
-            // Update the position of the current character in the map
-            mpp[currentChar] = end;
-
-            // Update the maximum length of substring without repeating characters
-            maxi = max(maxi, end - start + 1);
+            else
+            {
+                left = mpp[s[right]] + 1;
+                mpp[s[right]] = right;
+            }
+            
         }
-
-        return maxi;
+                return maxLength;
     }
 };
