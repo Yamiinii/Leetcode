ce535f65c3f1da12627ee945a2c1e25fc3d85b95
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-07-24 18:35:29 +0530

    Time: 6 ms (24.62%), Space: 8.6 MB (37.82%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,5 +1,19 @@
 class Solution {
 public:
+    int gcd(int a, int b) 
+{ 
+    // Find Minimum of a and b 
+    int result = min(a, b); 
+    while (result > 0) { 
+        if (a % result == 0 && b % result == 0) { 
+            break; 
+        } 
+        result--; 
+    } 
+  
+    // Return gcd of a and b 
+    return result; 
+}
     string gcdOfStrings(string str1, string str2) {
         return (str1 + str2 == str2 + str1)?str1.substr(0, gcd(size(str1),size(str2))): ""; 
     }
