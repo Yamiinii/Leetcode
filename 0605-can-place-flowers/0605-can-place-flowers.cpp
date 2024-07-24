a297a931eb67ab4dc36f98f188c882eaaf700a67
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-07-24 19:10:56 +0530

    Time: 10 ms (73.58%), Space: 23 MB (14.55%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,43 +1,31 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        if(flowerbed.size() == 1)
-        {
-            if(flowerbed[0] == 0 && n == 1)
-            {
+        if (flowerbed.size() == 1) {
+            if (flowerbed[0] == 0 && n == 1) {
                 return true;
-            }
-            else if((flowerbed[0] == 1 || flowerbed[0] == 0) && n == 0)
-            {
+            } else if ((flowerbed[0] == 1 || flowerbed[0] == 0) && n == 0) {
                 return true;
-            }
-            else
-            {
+            } else {
                 return false;
             }
         }
-        int count = 0;
-        int prev = flowerbed[0];
-        for(int i = 1 ; i < flowerbed.size();i++)
-        {
-          if(flowerbed[0] == 0 && flowerbed[1] == 0)
-          {
-              flowerbed[0] = 1;
-              count++; 
-          }
-          else if(flowerbed[i] == 0 && prev == 0)
-          {
-            count++;
-            flowerbed[i] = 1;
+        if(n==0)
+            return true;
+        
+        for (int i = 0; i < flowerbed.size(); i++) {
+            bool left = i == 0 || flowerbed[i - 1] == 0;
+            bool right = i == flowerbed.size() - 1 || flowerbed[i + 1] == 0;
             
-          }
-          else if(prev == 1 && flowerbed[i] == 1)
-          {
-            count--;
-          }
-          prev = flowerbed[i];
+            if (left && right && flowerbed[i] == 0) {
+                flowerbed[i] = 1; // Place a flower
+                n--; // Decrease the count of flowers to be placed
+                
+                if (n == 0)
+                    return true; // If we've placed enough flowers, return true
+            }
         }
         
-        return count>n-1;
+        return n == 0; // Return true if we've placed enough flowers, otherwise false
     }
 };
