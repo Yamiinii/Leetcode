d778a924ce8a73966b46599b892fe7ce0060383d
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-07-25 20:52:00 +0530

    Time: 19 ms (72.89%), Space: 19.8 MB (17.22%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,60 +1,39 @@
-class DisjointSet {
-    vector<int> rank, parent, size;
-
-public:
-    DisjointSet(int n) {
-        rank.resize(n + 1, 0);
-        size.resize(n + 1, 1);  // Initialize size of each set to 1
-        parent.resize(n + 1, 0);
-        for (int i = 0; i <= n; i++) {
-            parent[i] = i;
-        }
-    }
-
-    int findUpar(int node) {
-        if (node == parent[node])
-            return node;
-        return parent[node] = findUpar(parent[node]);
-    }
-
-    void unionByRank(int u, int v) {
-        int ulp_u = findUpar(u);
-        int ulp_v = findUpar(v);
-        if (ulp_u == ulp_v)
-            return;
-        if (rank[ulp_u] < rank[ulp_v]) {
-            parent[ulp_u] = ulp_v;
-            size[ulp_v] += size[ulp_u];  // Update the size of the merged set
-        } else if (rank[ulp_v] < rank[ulp_u]) {
-            parent[ulp_v] = ulp_u;
-            size[ulp_u] += size[ulp_v];  // Update the size of the merged set
-        } else {
-            parent[ulp_v] = ulp_u;
-            rank[ulp_u]++;
-            size[ulp_u] += size[ulp_v];  // Update the size of the merged set
-        }
-    }
-};
-
 class Solution {
 public:
+    void dfs(vector<vector<int>>& v, vector<int>& visited, int i) {
+        visited[i] = 1;
+        for (auto it : v[i]) {
+            if (!visited[it]) {
+                dfs(v, visited, it);
+            }
+        }
+    }
+    
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        DisjointSet ds(n);
-        set<int> s;
-
+        vector<vector<int>> v(n);  // Adjacency list for graph
+        
+        // Build adjacency list from isConnected matrix
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (isConnected[i][j] == 1) {
-                    ds.unionByRank(i, j);
+                if (i != j && isConnected[i][j] == 1) {
+                    v[i].push_back(j);
+                    v[j].push_back(i);
                 }
             }
         }
-
+        
+        vector<int> visited(n, 0);
+        int cnt = 0;  // Counter for connected components
+        
+        // Perform DFS to find connected components
         for (int i = 0; i < n; i++) {
-            s.insert(ds.findUpar(i));  // Insert ultimate parents of each node into the set
+            if (!visited[i]) {
+                cnt++;
+                dfs(v, visited, i);
+            }
         }
-
-        return s.size();
+        
+        return cnt;
     }
 };
