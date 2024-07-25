f07af95fff85ec54fdb25ef49608d924afc961d1
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-07-25 19:17:31 +0530

    Time: 28 ms (26.72%), Space: 33.5 MB (16.08%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -1,50 +1,68 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    TreeNode* deleteNode(TreeNode* root, int key) {
-        if (root == nullptr) return root; // Return null if root is null
-
-        // If key to be deleted is less than the root node, then it lies in left subtree
-        if (key < root->val) {
-            root->left = deleteNode(root->left, key);
-        }
-        // If key to be deleted is greater than the root node, then it lies in right subtree
-        else if (key > root->val) {
-            root->right = deleteNode(root->right, key);
-        }
-        // If key is same as root's key, then this is the node to be deleted
-        else {
-            // Node with only one child or no child
-            if (root->left == nullptr) {
-                TreeNode *temp = root->right;
-                delete root;
-                return temp;
-            }
-            else if (root->right == nullptr) {
-                TreeNode *temp = root->left;
-                delete root;
-                return temp;
-            }
-
-            // Node with two children: get the inorder successor (smallest in the right subtree)
-            TreeNode* temp = minValueNode(root->right);
-
-            // Copy the inorder successor's content to this node
-            root->val = temp->val;
-
-            // Delete the inorder successor
-            root->right = deleteNode(root->right, temp->val);
-        }
-        return root;
+    TreeNode* findlastright(TreeNode* root)
+    {
+        if(root->right==NULL)
+            return root;
+        return findlastright(root->right);
     }
-
-private:
-    TreeNode* minValueNode(TreeNode* node) {
-        TreeNode* current = node;
-
-        /* loop down to find the leftmost leaf */
-        while (current && current->left != nullptr)
-            current = current->left;
-
-        return current;
+    
+    TreeNode* helper(TreeNode* root)
+    {
+        if(root->left==NULL)
+            return root->right;
+        else if(root->right==NULL)
+            return root->left;
+        TreeNode* rightChild=root->right;
+        TreeNode* lastRight=findlastright(root->left);
+        lastRight->right=rightChild;
+        return root->left;
+    }
+    
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root==NULL)
+            return NULL;
+        
+        if(root->val==key)
+            return helper(root);
+        
+        TreeNode* dummy=root;
+        while(root!=NULL)
+        {
+            if(root->val>key)
+            {
+                if(root->left!=NULL && root->left->val==key)
+                {
+                    root->left=helper(root->left);
+                    break;
+                }
+                else
+                    root=root->left;
+            }
+            else
+            {
+                if(root->right!=NULL && root->right->val==key)
+                {
+                    root->right=helper(root->right);
+                    break;
+                }
+                else
+                {
+                    root=root->right;
+                }
+            }
+        }
+        return dummy;
     }
 };
