1b1380a5f400cae9dd3b6fd7aa7f7a3e959f4fad
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-07-28 15:17:30 +0530

    Time: 3 ms (66.31%), Space: 21.8 MB (6.11%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -11,27 +11,28 @@
  */
 class Solution {
 public:
-    TreeNode* buildTree(vector<int>& preorder,int prestart,int preend, vector<int>& inorder,int inStart,int inEnd,unordered_map<int,int> &mpp)
+    TreeNode* buildTree(vector<int> preorder,int prestart,int preend,vector<int> inorder,int instart,int inend,  unordered_map<int,int>& mpp)
     {
-           if(inStart>inEnd || prestart>preend)
+            if(instart>inend || prestart>preend)
             return NULL;
-        TreeNode* root=new TreeNode(preorder[prestart]);
-        int inRoot=mpp[root->val];
-        int numsleft=inRoot-inStart;
-        root->left=buildTree(preorder,prestart+1,prestart+numsleft,inorder,inStart,inRoot-1,mpp);
-        root->right=buildTree(preorder,prestart+numsleft+1,preend,inorder,inRoot+1,inEnd,mpp);
-        return root;
+            
+            TreeNode* root=new TreeNode(preorder[prestart]);
+            int inRoot=mpp[root->val];
+            int left=inRoot-instart;
+            root->left=buildTree(preorder,prestart+1,prestart+left,inorder,instart,inRoot-1,mpp);
+            root->right=buildTree(preorder,prestart+1+left,preend,inorder,inRoot+1,inend,mpp);
+            return root;
     }
     
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        vector<int> inorder;
-        inorder=preorder;
+        vector<int> inorder=preorder;
+        int n=preorder.size();
         sort(inorder.begin(),inorder.end());
         unordered_map<int,int> mpp;
-        int n=inorder.size();
         for(int i=0;i<n;i++)
+        {
             mpp[inorder[i]]=i;
-        
+        }
         return buildTree(preorder,0,n-1,inorder,0,n-1,mpp);
     }
 };
