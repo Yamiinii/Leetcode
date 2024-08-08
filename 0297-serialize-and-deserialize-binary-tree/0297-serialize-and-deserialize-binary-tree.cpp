6feeaad9ec0e4aa7b3c1360acce31d237510a110
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-08-08 18:32:00 +0530

    Time: 20 ms (90.57%), Space: 30.3 MB (43.42%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
@@ -1,10 +1,3 @@
-#include <string>
-#include <sstream>
-#include <queue>
-#include <utility>
-
-using namespace std;
-
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -16,60 +9,59 @@ using namespace std;
  */
 class Codec {
 public:
-    // Serialize the binary tree to a string
-    string serialize(TreeNode* root) {
-        if (!root) {
-            return "";
-        }
 
-        stringstream ss;
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        if(root==nullptr)
+            return "";
         queue<TreeNode*> q;
         q.push(root);
-        while (!q.empty()) {
-            TreeNode* curNode = q.front();
+        stringstream ss;
+        while(!q.empty())
+        {
+            TreeNode* front=q.front();
             q.pop();
-            if (curNode == nullptr) {
-                ss << "#,";
-            } else {
-                ss << curNode->val << ",";
-                q.push(curNode->left);
-                q.push(curNode->right);
+            if(front==NULL)
+                 ss << "#,";
+            else
+            {
+                ss << front->val << ",";
+                 q.push(front->left);
+                q.push(front->right);
             }
         }
+        
         return ss.str();
     }
 
-    // Deserialize the string to a binary tree
+    // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
-        if (data.empty()) {
-            return nullptr;
-        }
-
-        stringstream ss(data);
-        string str;
-        getline(ss, str, ',');
-        if (str == "#") {
-            return nullptr;
-        }
+        if(data.empty())
+            return NULL;
         
-        TreeNode* root = new TreeNode(stoi(str));
+        stringstream ss(data);
+        string s;
+        getline(ss,s,',');
+        if(s=="#")
+            return NULL;
+          TreeNode* root = new TreeNode(stoi(s));
         queue<TreeNode*> q;
         q.push(root);
-        
-        while (!q.empty()) {
-            TreeNode* node = q.front();
+        while(!q.empty())
+        {
+            TreeNode* front=q.front();
             q.pop();
-            
-            getline(ss, str, ',');
-            if (str != "#") {
-                node->left = new TreeNode(stoi(str));
-                q.push(node->left);
+            getline(ss,s,',');
+            if(s!="#")
+            {
+                front->left=new TreeNode(stoi(s));
+                q.push(front->left);
             }
-            
-            getline(ss, str, ',');
-            if (str != "#") {
-                node->right = new TreeNode(stoi(str));
-                q.push(node->right);
+            getline(ss,s,',');
+            if(s!="#")
+            {
+                front->right=new TreeNode(stoi(s));
+                q.push(front->right);
             }
         }
         
