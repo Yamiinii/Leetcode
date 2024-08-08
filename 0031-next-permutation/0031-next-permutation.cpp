a2e0e8846c0a40e0db284fb04b23290e2148ccfa
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-08-08 12:57:03 +0530

    Time: 0 ms (100.00%), Space: 14.9 MB (25.28%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n = nums.size();
-        int dip = -1;
-        
-        // Step 1: Find the largest index dip such that nums[dip] < nums[dip+1]
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
-                dip = i;
+        int n=nums.size();
+        int dip=-1;
+        for(int i=n-2;i>=0;i--)
+        {
+            if(nums[i]<nums[i+1])
+            {
+                dip=i;
                 break;
             }
         }
         
-        // Step 2: If dip is -1, nums is the last permutation, so reverse it to get the first permutation
-        if (dip == -1) {
-            reverse(nums.begin(), nums.end());
+        if(dip==-1)
+        {
+            reverse(nums.begin(),nums.end());
             return;
         }
         
-        // Step 3: Find the smallest number greater than nums[dip] from dip+1 to n-1
         int mini = nums[dip + 1];
         int number = dip + 1;
         
@@ -29,10 +28,8 @@ public:
             }
         }
         
-        // Step 4: Swap nums[dip] and nums[number]
-        swap(nums[dip], nums[number]);
+        swap(nums[dip],nums[number]);
         
-        // Step 5: Sort nums from dip+1 to the end to get the next permutation
-        sort(nums.begin() + dip + 1, nums.end());
+        sort(nums.begin()+dip+1,nums.end());
     }
 };
