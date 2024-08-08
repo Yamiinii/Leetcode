07466a09df5573a2859325e8e3b815a2db3c61e0
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-08-08 14:17:12 +0530

    Time: 14 ms (85.57%), Space: 16.7 MB (22.48%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
-            //DFS
-    bool dfs(vector<vector<int>>& graph,int i,vector<int> &coloured,int color)
-    {
-        coloured[i]=color;
-        for(auto it:graph[i])
-        {if(coloured[it]==-1)
-               { if(dfs(graph,it,coloured,1-color)==false)
-                    return false;
-               } else if(coloured[it]==coloured[i])
-                return false;
-        }
-        return true;
-    }
+    //linear graphs with no cycle are always biparatite
+    //colouring adj differently
+    //any graph even length cycle
     bool isBipartite(vector<vector<int>>& graph) {
+        vector<int> colors(graph.size(),-1);
+    for (int start = 0; start < graph.size(); ++start) {
+            if (colors[start] == -1) { // If this node hasn't been colored yet
+                queue<int> q;
+                q.push(start);
+                colors[start] = 0; // Start coloring with 0
 
-        vector<int> coloured(graph.size(),-1);
-        for(int i=0;i<graph.size();i++)
-        {
-            if(coloured[i]==-1)
-               { if(dfs(graph,i,coloured,1)==false)
-                    return false;}
-            
+                while (!q.empty()) {
+                    int front = q.front();
+                    q.pop();
+
+                    for (auto adj : graph[front]) {
+                        if (colors[adj] == -1) {
+                            // Color with the opposite color of the current node
+                            colors[adj] = !colors[front];
+                            q.push(adj);
+                        } else if (colors[adj] == colors[front]) {
+                            // If adjacent node has the same color, it's not bipartite
+                            return false;
+                        }
+                    }
+                }
+            }
         }
+
         return true;
-        
     }
 };
