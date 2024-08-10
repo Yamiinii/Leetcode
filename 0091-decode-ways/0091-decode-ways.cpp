31f1239f8128ec628da34c66aa0d7150c2e4c54f
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-08-10 21:32:02 +0530

    Time: 6 ms (10.39%), Space: 10.8 MB (15.09%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
- int ways(const std::string& s, int i, std::vector<int>& dp) {
-        int n = s.size();
-        if (i == n) {
+    int ways(string s ,vector<int> &dp,int i)
+    {
+        if(i==s.size())
             return 1;
-        }
-        if (s[i] == '0') {
+        if(s.size()==0)
             return 0;
-        }
-        if (dp[i] != -1) {
+        if(s[i]=='0')
+            return 0;
+        if(dp[i]!=-1)
             return dp[i];
-        }
-        int ans = ways(s, i + 1, dp);
-        if (i < n - 1 && (s[i] == '1' || (s[i] == '2' && s[i + 1] <= '6'))) {
-            ans += ways(s, i + 2, dp);
-        }
-        dp[i] = ans;
-        return ans;
+        //considering only a single char
+        int ans=ways(s,dp,i+1);
+        if(i<s.size()-1 && (s[i]=='1' || s[i]=='2' && s[i+1]<='6'))
+            ans+=ways(s,dp,i+2);
+        
+        return dp[i]=ans;
     }
     
-    int numDecodings(const std::string& s) {
-        if (s.empty()) {
-            return 0;
-        }
-        std::vector<int> dp(s.size(), -1);
-        return ways(s, 0, dp);
+    int numDecodings(string s) {
+        int n=s.size();
+        vector<int> dp(n,-1);
+        return ways(s,dp,0);
     }
 };
