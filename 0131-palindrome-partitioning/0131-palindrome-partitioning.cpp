c977a85dd211b6145e7ab611143689079eb8534f
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-08-11 22:41:41 +0530

    Time: 95 ms (56.67%), Space: 61.3 MB (46.65%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,54 +1,41 @@
 class Solution {
 public:
-    int isPalindrome(string s,int i,int j)
+    bool isPalindrome(string s,int start,int end)
     {
-        while(i<=j)
+        while(start<=end)
         {
-            if(s[i]!=s[j])
+            if(s[start]!=s[end])
                 return false;
-            i++;
-            j--;
+            start++;
+            end--;
         }
+        
         return true;
     }
     
-//     int solve(string s,int i,int j)
-//     {
-//         if(i>j)
-//             return 0;
-//         if(isPalindrome(s,i,j))
-//             return 0;
-//         int mini=INT_MAX;
-//         for(int k=i;k<j;k++)
-//         {
-//             int temp=solve(s,i,k)+solve(s,k+1,j)+1;
-//             int mini=min(temp,mini);
-//         }
-//         return mini;
-//     }
-void partitionHelper(string s, int start, vector<string>& current, vector<vector<string>>& result) {
-    if (start == s.size()) {
-        result.push_back(current);
-        return;
-    }
-    
-    for (int end = start; end < s.size(); end++) {
-        if (isPalindrome(s, start, end)) {
-            current.push_back(s.substr(start, end - start + 1));
-            partitionHelper(s, end + 1, current, result);
-            current.pop_back();
+    void rec(string s,vector<vector<string>>& ans,vector<string> &v,int start)
+    {
+            if(start==s.size())
+            {
+                ans.push_back(v);
+                return;
+            }
+        
+        for(int end=start;end<s.size();end++)
+        {
+            if(isPalindrome(s,start,end))
+            {
+                v.push_back(s.substr(start,end-start+1));
+                rec(s,ans,v,end+1);
+                v.pop_back();
+            }
         }
     }
-}
-
     
     vector<vector<string>> partition(string s) {
-        // int n=s.size();
-        // return solve(s,1,n-1);
-        int n=s.size();
         vector<vector<string>> ans;
-        vector<string> current;
-        partitionHelper(s,0,current,ans);
+        vector<string> v;
+        rec(s,ans,v,0);
         return ans;
     }
 };
