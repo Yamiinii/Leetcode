c281dd776fd58d649956354b76341cdb56b7b8a4
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-08-13 00:11:50 +0530

    Time: 29 ms (36.29%), Space: 24.9 MB (17.04%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
@@ -1,36 +1,29 @@
-#include <vector>
-#include <queue>
-
 class KthLargest {
 public:
-    int K;  // The k value
-    std::priority_queue<int, std::vector<int>, std::greater<int>> pq; // Min-heap
-
-    // Constructor
-    KthLargest(int k, std::vector<int>& nums) : K(k) {
-        // Insert elements into the min-heap
+    KthLargest(int k, vector<int>& nums) : k(k) {
         for (int num : nums) {
-            pq.push(num);
-            // Maintain only k largest elements
-            if (pq.size() > K) {
-                pq.pop();
+            if (minHeap.size() < k) {
+                minHeap.push(num);
+            } else if (num > minHeap.top()) {
+                minHeap.push(num);
+                if (minHeap.size() > k) {
+                    minHeap.pop();
+                }
             }
         }
     }
-    
-    // Add a new value and return the k-th largest element
-    int add(int val) {
-        pq.push(val); // Add the new value
-        // Maintain only k largest elements
-        if (pq.size() > K) {
-            pq.pop();
-        }
-        return pq.top(); // The top of the min-heap is the k-th largest element
-    }
-};
 
-/**
- * Your KthLargest object will be instantiated and called as such:
- * KthLargest* obj = new KthLargest(k, nums);
- * int param_1 = obj->add(val);
- */
+    int add(int val) {
+        if (minHeap.size() < k) {
+            minHeap.push(val);
+        } else if (val > minHeap.top()) {
+            minHeap.push(val);
+            minHeap.pop();
+        }
+        return minHeap.top();
+    }
+
+private:
+    int k;
+    priority_queue<int, vector<int>, greater<int>> minHeap;  // Min-heap to store the k largest elements
+};
