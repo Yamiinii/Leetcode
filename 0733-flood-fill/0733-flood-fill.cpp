1f1fe386b17e7988b32f363d8008104fb31dfa8e
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-08-13 17:18:21 +0530

    Time: 13 ms (7.85%), Space: 17.2 MB (30.19%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,25 +1,34 @@
 class Solution {
 public:
-    
-  void  dfs(vector<vector<int>>& image, int sr, int sc, int color,int in_color)
+    void bfs(vector<vector<int>>& image, int sr, int sc, int color,int old)
     {
-            image[sr][sc]=color;
+          int rows = image.size();
+        int cols = image[0].size();
+        queue<pair<int, int>> q;
+        q.push({sr, sc});
+        image[sr][sc] = color;
         int x[4]={1,-1,0,0};
         int y[4]={0,0,1,-1};
-        for(int i=0;i<4;i++)
-        {
-            int nx=sr+x[i];
-            int ny=sc+y[i];
-            if (nx >= 0 && nx < image.size() && ny >= 0 && ny < image[0].size() && image[nx][ny] == in_color && image[nx][ny] != color)
-
-               dfs(image,nx,ny,color,in_color); 
+               while (!q.empty()) {
+            auto [r, c] = q.front();
+            q.pop();
+            
+            for (int i = 0; i < 4; ++i) {
+                int newR = r + x[i];
+                int newC = c + y[i];
+                
+                if (newR >= 0 && newR < rows && newC >= 0 && newC < cols &&
+                    image[newR][newC] == old && image[newR][newC] != color) {
+                    image[newR][newC] = color;
+                    q.push({newR, newC});
+                }
+            }
         }
     }
     
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int in_color=image[sr][sc];
-        vector<vector<int>> ans=image;
-        dfs(ans,sr,sc,color,in_color);
-        return ans;
+        int old=image[sr][sc];
+        bfs(image,sr,sc,color,old);
+            return image;
     }
 };
