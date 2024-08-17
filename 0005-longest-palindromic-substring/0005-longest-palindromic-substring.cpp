b7a302d6570db5807cfd9eaef310aa12b6e3e2d4
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-08-17 18:51:05 +0530

    Time: 136 ms (36.83%), Space: 26.7 MB (37.75%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,51 +1,45 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n = s.size();
-        if (n == 0) return "";
-
-        // Initialize a 2D dp table where dp[i][j] will be true if s[i...j] is a palindrome
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
-
-        int start = 0;   // Starting index of the longest palindrome substring
-        int maxLen = 1;  // Length of the longest palindrome substring (minimum is 1 for single character)
-
-        // All substrings of length 1 are palindromes
-        for (int i = 0; i < n; ++i) {
-            dp[i][i] = true;
-        }
-
-        // Check for substrings of length 2
-        for (int i = 0; i < n - 1; ++i) {
-            if (s[i] == s[i + 1]) {
-                dp[i][i + 1] = true;
-                start = i;
-                maxLen = 2;
+        int n=s.size();
+        if(n==0)
+            return "";
+        if(n==1)
+            return s;
+        
+        vector<vector<bool>> dp(n+1,vector<bool>(n+1,false));
+        
+        for(int i=0;i<n;i++)
+            dp[i][i]=true;
+        
+        int start=0;
+        int maxlen=1;
+        
+        for(int i=0;i<n-1;i++)
+        {
+            if(s[i]==s[i+1])
+            {
+                dp[i][i+1]=true;
+                start=i;
+                maxlen=2;
             }
         }
-
-        // Check for substrings of length > 2
-        for (int len = 3; len <= n; ++len) {
-            for (int i = 0; i <= n - len; ++i) {
-                int j = i + len - 1;  // Ending index of substring
-
-                // Check if s[i...j] is a palindrome
-                if (s[i] == s[j] && dp[i + 1][j - 1]) {
-                    dp[i][j] = true;
-                    if (len > maxLen) {
-                        start = i;
-                        maxLen = len;
-                    }
+        
+        for(int len=3;len<=n;len++)
+        {
+            for(int i=0;i<=n-len;i++)
+            {
+                int j=i+len-1;
+                if(s[i]==s[j] && dp[i+1][j-1])
+                {
+                    dp[i][j]=true;
+                    if(len>maxlen)
+                {start=i;
+                maxlen=len;}
                 }
             }
         }
-
-        // Extract the longest palindrome substring from s using start and maxLen
-        return s.substr(start, maxLen);
+        
+        return s.substr(start,maxlen);
     }
 };
