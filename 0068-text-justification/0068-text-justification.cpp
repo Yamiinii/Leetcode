e075821c08dd950a84e65b9e0a1d7c6404a56ed0
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-08-17 12:16:37 +0530

    Time: 5 ms (11.33%), Space: 9.3 MB (50.39%) - LeetHub

--- 0068-text-justification/0068-text-justification.cpp
+++ 0068-text-justification/0068-text-justification.cpp
@@ -1,65 +1,64 @@
 class Solution {
 public:
-    vector<string> fullJustify(vector<string>& words, int maxWidth) {
-        vector<string> justified;
-        vector<string> wordsline;
-        int width = 0;
-        int spacing, remainder;
-        for (int k = 0; k < words.size(); k++) {
-            // Greedy
-            width += words[k].size();
-            if (width > maxWidth - wordsline.size()) {
-                width -= words[k].size();
-                if (wordsline.size() == 1) {
-                    spacing = -1;
-                }
-                else {
-                    spacing = (maxWidth - width) / (wordsline.size() - 1);
-                    remainder = (maxWidth - width) % (wordsline.size() - 1);
-                }
-                cout << "Line:\t" << width << "/" << maxWidth << " " << spacing << "R" << remainder << "\n";
-                stringstream ss;
-                width = maxWidth;
-                for (int k = 0; k < wordsline.size(); k++) {
-                    if (k) {
-                        ss << string(spacing, ' ');
-                        width -= spacing;
-                        if (remainder) {
-                            ss << ' ';
-                            remainder--;
-                            width--;
-                        }
-                    }
-                    ss << wordsline[k];
-                    width -= wordsline[k].size();
-                }
-                ss << string(width, ' ');
-                justified.push_back(ss.str());
-                cout << ss.str() << "\n\n";
-                wordsline.clear();
-                width = 0;
-                k--;
-            }
-            else {
-                wordsline.push_back(words[k]);
-            }
-        }
-        // Last line
-        cout << "Last:\t" << width << "/" << maxWidth << " " << spacing << "R" << remainder << "\n";
-        stringstream ss;
-        width = maxWidth;
-        for (int k = 0; k < wordsline.size(); k++) {
-            if (k) {
-                ss << " ";
-                width--;
-            }
-            ss << wordsline[k];
-            width -= wordsline[k].size();
-        }
-        ss << string(width, ' ');
-        justified.push_back(ss.str());
-        cout << ss.str() << "\n\n";
+    string find_line(int i,int j,int eachblankspace,int extrablankspace,vector<string>& words,int maxi)
+    {
+        string line;
         
-        return justified;
+        for(int k=i;k<j;k++)
+        {
+            line+=words[k];
+            
+            if(k==j-1)
+                continue;
+            
+            for(int z=1;z<=eachblankspace;z++)
+                line+=" ";
+            
+            if(extrablankspace>0)
+            {
+                line+=" ";
+                extrablankspace--;
+            }
+        }
+        
+        while(line.length()<maxi)
+            line+=" ";
+        
+        return line;
+    }
+    
+    vector<string> fullJustify(vector<string>& words, int maxWidth) {
+        vector<string> ans;
+        int i=0;
+        int n=words.size();
+        int maxi=maxWidth;
+        while(i<n)
+        {
+            int lettercnt=words[i].size();
+            int space=0;
+            int j=i+1;
+            while(j<n && words[j].length()+1+lettercnt+space<=maxi)
+            {
+                lettercnt+=words[j].size();
+                space++;
+                j++;
+            }
+            
+            int remainingspace=maxi-lettercnt;
+            int eachblankspace=space==0?0:remainingspace/space;
+            int extrablankspace=space==0?0:remainingspace%space;
+            
+            if(j==n)
+            {
+                eachblankspace=1;
+                extrablankspace=0;
+            }
+            
+            ans.push_back(find_line(i,j,eachblankspace,extrablankspace,words,maxi));
+            
+            i=j;
+        }
+        
+        return ans;
     }
 };
