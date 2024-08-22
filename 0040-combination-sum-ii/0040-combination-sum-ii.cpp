456951a546b67a36e34b591d6c552a111eacdd8d
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-08-22 11:39:08 +0530

    Time: 5 ms (39.04%), Space: 14.2 MB (22.96%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -5,31 +5,36 @@ using namespace std;
 
 class Solution {
 public:
-    void findall(vector<int>& can, int target, vector<vector<int>>& ans, vector<int>& v, int idx) {
-        if (target == 0) {
+    void rec(const std::vector<int>& candidates, int target, std::vector<std::vector<int>>& ans, std::vector<int> v, int idx) {
+        if (target==0) {
             ans.push_back(v);
             return;
         }
-
-        for (int i = idx; i < can.size(); ++i) {
-            // Skip duplicates
-            if (i > idx && can[i] == can[i - 1])
+        
+        if(target<0)
+            return;
+        
+        for (int i = idx; i < candidates.size(); ++i) {
+            // // Skip duplicates
+            if (i > idx && candidates[i] == candidates[i - 1])
                 continue;
 
-            if (can[i] <= target) {
-                v.push_back(can[i]);
-                findall(can, target - can[i], ans, v, i + 1); // Increment idx for next recursive call
+            if (candidates[i] <= target) {
+                v.push_back(candidates[i]);
+                rec(candidates, target-candidates[i], ans,v, i+1); // Increment idx for next recursive call
                 v.pop_back();
             }
         }
     }
 
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
-        vector<int> v;
-         
-        sort(candidates.begin(), candidates.end()); // Sorting candidates array
-        findall(candidates, target, ans, v, 0); // Start recursive call with idx = 0
+     std::vector<std::vector<int>> ans;
+        std::vector<int> v;
+        
+        // Sort the candidates to handle duplicate combinations more effectively
+        std::sort(candidates.begin(), candidates.end());
+        
+        rec(candidates, target, ans, v, 0);
         return ans;
     }
 };
