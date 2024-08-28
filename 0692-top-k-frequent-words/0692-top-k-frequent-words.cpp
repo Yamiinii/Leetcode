2741d39687af6edabb9d35cd8fe95a160dbaf923
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-08-28 22:59:05 +0530

    Time: 4 ms (95.85%), Space: 17.8 MB (18.07%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
-    struct Compare {
-        bool operator()(const pair<int, string>& a, const pair<int, string>& b) {
-            if (a.first == b.first) {
-                return a.second > b.second;  // Min-heap based on lexicographical order
-            }
-            // Otherwise, compare based on frequency
-            return a.first < b.first; 
-        }
+    class compare{
+        public:
+      bool operator()(const pair<int,string>& a,const pair<int,string> &b)
+      {
+          if(a.first==b.first)
+              return a.second>b.second;
+          
+          return a.first<b.first;
+      }
     };
     
     vector<string> topKFrequent(vector<string>& words, int k) {
         unordered_map<string,int> mpp;
+        vector<string>v;
         for(auto it:words)
         {
             mpp[it]++;
         }
-        priority_queue<pair<int, string>, vector<pair<int, string>>, Compare> pq;
+        priority_queue<pair<int,string>,vector<pair<int,string>>,compare> pq;
         for(auto it:mpp)
         {
             pq.push({it.second,it.first});
         }
-        vector<string> ans;
-          while (k-- > 0 && !pq.empty()) {
-            ans.push_back(pq.top().second);
+        while(k-- && !pq.empty())
+        {
+            v.push_back(pq.top().second);
             pq.pop();
         }
-        return ans;
+        return v;
     }
 };
