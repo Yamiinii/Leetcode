5b4b23d77f6f35f8c074804c2818d44e844490f2
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-08-28 19:09:53 +0530

    Time: 2 ms (53.97%), Space: 11.2 MB (59.70%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.cpp
+++ 0399-evaluate-division/0399-evaluate-division.cpp
@@ -1,52 +1,63 @@
 class Solution {
 public:
-    double bfs(string src,string dest, unordered_map<string,vector<pair<string,double>>> &adj)
-    {
-        queue<pair<string,double>> q;
-        q.push({src,1.0});
-        unordered_map<string,int> vis;
-        while(!q.empty())
-        {
-            int size=q.size();
-            while(size--)
-            {
-                auto x=q.front();
-                q.pop();
-                string node=x.first;
-                double d=x.second;
-                vis[node]++;
-                if(node==dest)
-                    return d;
-                for(auto & c:adj[node])
-                {
-                    if(vis.find(c.first)==vis.end())
-                        q.push({c.first,d*c.second});
+    double bfs(const string& s, const string& d, 
+               const unordered_map<string, vector<pair<string, double>>>& mpp, 
+               unordered_map<string, bool>& vis) {
+        if (s == d) return 1.0; // Special case where source is the destination
+        
+        queue<pair<string, double>> q;
+        q.push({s, 1.0});
+        vis[s] = true; // Mark start node as visited
+
+        while (!q.empty()) {
+            auto front = q.front();
+            q.pop();
+
+            string node = front.first;
+            double value = front.second;
+
+            if (node == d) {
+                return value;
+            }
+
+            for (const auto& neighbor : mpp.at(node)) {
+                if (vis.find(neighbor.first) == vis.end()) {
+                    vis[neighbor.first] = true;
+                    q.push({neighbor.first, value * neighbor.second});
                 }
             }
         }
-        return -1;
+
+        return -1.0; // If no path was found
     }
     
-    vector<double> calcEquation(vector<vector<string>>& a, vector<double>& b, vector<vector<string>>& q) {
-        unordered_map<string,vector<pair<string,double>>> adj;
-        int n=a.size();
-        for(int i=0;i<n;i++)
-        {
-            adj[a[i][0]].push_back({a[i][1],b[i]});
-            adj[a[i][1]].push_back({a[i][0],1/b[i]});
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        unordered_map<string, vector<pair<string, double>>> mpp;
+        int n = equations.size();
+        
+        for (int i = 0; i < n; ++i) {
+            const string& u = equations[i][0];
+            const string& v = equations[i][1];
+            double value = values[i];
+            mpp[u].push_back({v, value});
+            mpp[v].push_back({u, 1.0 / value});
         }
         
         vector<double> ans;
         
-        for(auto& x:q)
-        {
-            if(adj.find(x[0])==adj.end() || adj.find(x[1])==adj.end())
-                ans.push_back(-1);
-            else
-                ans.push_back(bfs(x[0],x[1],adj));
+        for (const auto& query : queries) {
+            const string& start = query[0];
+            const string& end = query[1];
+            
+            unordered_map<string, bool> vis;
+            
+            if (mpp.find(start) == mpp.end() || mpp.find(end) == mpp.end()) {
+                ans.push_back(-1.0);
+            } else {
+                ans.push_back(bfs(start, end, mpp, vis));
+            }
         }
         
         return ans;
-        
     }
 };
