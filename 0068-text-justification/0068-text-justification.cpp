77f16adcbe4a4bc6257bacd8b6bc8d6cc849855f
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-08-29 16:53:38 +0530

    Time: 4 ms (34.56%), Space: 9.2 MB (81.37%) - LeetHub

--- 0068-text-justification/0068-text-justification.cpp
+++ 0068-text-justification/0068-text-justification.cpp
@@ -1,61 +1,56 @@
 class Solution {
 public:
-    string find_line(int i,int j,int eachblankspace,int extrablankspace,vector<string>& words,int maxi)
+    string addline(vector<string>& words, int maxWidth,int i,int j,int eachspace,int leftmargin)
     {
         string line;
         
         for(int k=i;k<j;k++)
         {
             line+=words[k];
-            
             if(k==j-1)
                 continue;
             
-            for(int z=1;z<=eachblankspace;z++)
+            for(int l=0;l<eachspace;l++)
                 line+=" ";
             
-            if(extrablankspace>0)
+            if(leftmargin>0)
             {
                 line+=" ";
-                extrablankspace--;
+                leftmargin--;
             }
         }
-        
-        while(line.length()<maxi)
+        while(line.length()<maxWidth)
             line+=" ";
         
         return line;
     }
     
     vector<string> fullJustify(vector<string>& words, int maxWidth) {
+        int n=words.size();
         vector<string> ans;
         int i=0;
-        int n=words.size();
-        int maxi=maxWidth;
         while(i<n)
         {
             int lettercnt=words[i].size();
             int space=0;
             int j=i+1;
-            while(j<n && words[j].length()+1+lettercnt+space<=maxi)
+            while(j<n && words[j].size()+1+space+lettercnt<=maxWidth)
             {
                 lettercnt+=words[j].size();
                 space++;
                 j++;
             }
             
-            int remainingspace=maxi-lettercnt;
-            int eachblankspace=space==0?0:remainingspace/space;
-            int extrablankspace=space==0?0:remainingspace%space;
+            int remain=maxWidth-lettercnt;
+            int eachspace=space==0?0:remain/space;
+            int leftmargin=space==0?0:remain%space;
             
             if(j==n)
             {
-                eachblankspace=1;
-                extrablankspace=0;
+                eachspace=1;
+                leftmargin=0;
             }
-            
-            ans.push_back(find_line(i,j,eachblankspace,extrablankspace,words,maxi));
-            
+            ans.push_back(addline(words,maxWidth,i,j,eachspace,leftmargin));
             i=j;
         }
         
