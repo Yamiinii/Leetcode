6ae14921ff3b0d783e85b9515bc9871bf7d132e7
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-08-30 18:14:24 +0530

    Time: 6 ms (31.68%), Space: 11.5 MB (70.87%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -13,7 +13,7 @@ public:
             {
                 if(nums[mid-1]<nums[mid] && nums[mid+1]<nums[mid])
                     return mid;
-                  else if (nums[mid-1]>nums[mid] && nums[mid+1] )
+                  else if (nums[mid-1]>nums[mid]  )
                     end=mid-1;
                 else
                     start=mid+1;
