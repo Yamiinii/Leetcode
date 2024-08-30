a65a03c6476e7b491b4d3114c2e72df03acf3f7e
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-08-30 18:40:08 +0530

    Time: 160 ms (40.87%), Space: 105.6 MB (79.67%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -2,24 +2,21 @@ class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k) {
         sort(nums.begin(),nums.end());
-        long maxi=0;
+        int n=nums.size();
         int left=0;
         int right=0;
+            long maxi=0;
         long total=0;
-        int n=nums.size();
         while(right<n)
         {
             total+=nums[right];
-            while(nums[right]*static_cast<long>(right-left+1)>total+k)
+           while(nums[right]*static_cast<long>(right-left+1)>total+k)
             {
                 total-=nums[left];
                 left++;
             }
-            
-            maxi=max(maxi,static_cast<long>(right-left+1));
-            right++;
+                        maxi=max(maxi,static_cast<long>(right-left+1));            right++;
         }
-        
-        return static_cast<int> (maxi);
+        return static_cast<int>(maxi);
     }
 };
