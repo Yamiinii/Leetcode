de9bd29ab7ab988b1ad5287d1387a2ea0db8ff40
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-08-30 09:47:03 +0530

    Time: 0 ms (100.00%), Space: 12.9 MB (13.15%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -2,43 +2,40 @@ class Solution {
 public:
     string decodeString(string s) {
         stack<char> st;
-        string ans = ""; // Initialize ans to store the decoded string
-        
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] != ']') {
-                st.push(s[i]); // Push characters onto stack until we encounter ']'
-            } else {
-                string substr = ""; // To store the substring that needs to be repeated
-                
-                // Pop characters from stack until we encounter '['
-                while (!st.empty() && st.top() != '[') {
-                    substr = st.top() + substr;
+        string ans="";
+        for(int i=0;i<s.size();i++)
+        {
+            string substr;
+            if(s[i]!=']')
+                st.push(s[i]);
+            else
+            {
+                while(!st.empty() && st.top()!='[')
+                {
+                    substr=st.top()+substr;
                     st.pop();
                 }
-                st.pop(); // Remove '[' from stack
-                
-                string num = ""; // To store the number that indicates repetition
-                
-                // Pop characters from stack until we encounter a non-digit
-                while (!st.empty() && isdigit(st.top())) {
-                    num = st.top() + num;
+                st.pop();
+                string num;
+                while(!st.empty() && isdigit(st.top()))
+                {
+                    num=st.top()+num;
                     st.pop();
                 }
                 
-                // Convert num from string to integer
-                int repeatCount = stoi(num);
-                
-                // Repeat the substring 'repeatCount' times and push it back to stack
-                for (int j = 0; j < repeatCount; j++) {
-                    for (char ch : substr) {
+                int num2=stoi(num);
+                for(int j=0;j<num2;j++)
+                {
+                   for (char ch : substr) {
                         st.push(ch);
                     }
                 }
             }
+            
+            
         }
         
-        // Build the final decoded string from the stack
-        while (!st.empty()) {
+         while (!st.empty()) {
             ans = st.top() + ans;
             st.pop();
         }
