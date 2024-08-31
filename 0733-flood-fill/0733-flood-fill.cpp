86430d9f8f8daf307ef680b819f101076208763d
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-08-31 17:47:54 +0530

    Time: 12 ms (12.47%), Space: 16.8 MB (79.71%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
-    void bfs(vector<vector<int>>& image, int sr, int sc, int color,int old)
-    {
-          int rows = image.size();
-        int cols = image[0].size();
-        queue<pair<int, int>> q;
-        q.push({sr, sc});
+void dfs(vector<vector<int>>& image, int sr, int sc, int color, int org) {
+        // Base case to stop recursion
+        if (sr < 0 || sr >= image.size() || sc < 0 || sc >= image[0].size() || image[sr][sc] != org) {
+            return;
+        }
+        
+        // Color the current cell
         image[sr][sc] = color;
-        int x[4]={1,-1,0,0};
-        int y[4]={0,0,1,-1};
-               while (!q.empty()) {
-            auto [r, c] = q.front();
-            q.pop();
+
+        // Define the directions for 4-neighbor cells
+        int x[4] = {1, -1, 0, 0};
+        int y[4] = {0, 0, 1, -1};
+        
+        // Recurse for all 4 adjacent cells
+        for (int k = 0; k < 4; k++) {
+            int xi = sr + x[k];
+            int yi = sc + y[k];
             
-            for (int i = 0; i < 4; ++i) {
-                int newR = r + x[i];
-                int newC = c + y[i];
-                
-                if (newR >= 0 && newR < rows && newC >= 0 && newC < cols &&
-                    image[newR][newC] == old && image[newR][newC] != color) {
-                    image[newR][newC] = color;
-                    q.push({newR, newC});
-                }
-            }
+            // Recursive DFS call
+            dfs(image, xi, yi, color, org);
         }
     }
     
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int old=image[sr][sc];
-        bfs(image,sr,sc,color,old);
-            return image;
+  int org = image[sr][sc];
+        if (org != color) { // Check if the color is different to avoid unnecessary operations
+            dfs(image, sr, sc, color, org);
+        }
+        return image;
     }
 };
