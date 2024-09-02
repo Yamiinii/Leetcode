f88786cdca808c6ae2e78a3d17b6c5d4cd0cf913
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-09-02 19:46:14 +0530

    Time: 107 ms (83.18%), Space: 52.3 MB (71.95%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,42 +1,48 @@
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
-    //bfs kahn
-    vector<int> eventualSafeNodes(vector<vector<int>>& adj) {
-        int V=adj.size();
-        vector<int> adjRev[V];
-	   vector<int> indegree(V, 0);
-	    for(int i=0;i<V;i++)
-	    {
-	        for(auto it:adj[i])
-	        {
-                adjRev[it].push_back(i);
-	            indegree[i]++;
-	        }
-	    }
-	    
-	    queue<int> q;
-	    for(int i=0;i<V;i++)
-	    {
-	        if(indegree[i]==0)
-	        q.push(i);
-	    }
-	    
-	    vector<int> topo;
-	    while(!q.empty())
-	    {
-	        int front=q.front();
-	        q.pop();
-	        topo.push_back(front);
-	        
-	        for(auto it:adjRev[front])
-	        {
-	            indegree[it]--;
-	            if(indegree[it]==0)
-	            q.push(it);
-	        }
-	    }
-        sort(topo.begin(),topo.end());
-	    return topo;
+    bool dfs(vector<vector<int>>& adj, vector<int>& vis, vector<int>& path, int i, vector<int>& safeNodes) {
+        vis[i] = 1;  // Mark node as visited
+        path[i] = 1; // Mark node as part of the current recursion stack
         
+        for (auto neighbor : adj[i]) {
+            if (!vis[neighbor]) {
+                if (dfs(adj, vis, path, neighbor, safeNodes)) {
+                    // If neighbor is part of a cycle, current node is also part of a cycle
+                    return true;
+                }
+            } else if (path[neighbor]) {
+                // If neighbor is in the current recursion stack, a cycle is detected
+                return true;
+            }
+        }
+        
+        path[i] = 0; // Unmark node as part of the current recursion stack
+        safeNodes[i] = 1; // Mark node as safe
+        return false; // No cycle found from this node
+    }
+    
+    vector<int> eventualSafeNodes(vector<vector<int>>& adj) {
+        int V = adj.size();
+        vector<int> vis(V, 0); // 0: not visited, 1: visited
+        vector<int> path(V, 0); // 0: not in current path, 1: in current path
+        vector<int> safeNodes(V, 0); // 0: not safe, 1: safe
+        
+        for (int i = 0; i < V; ++i) {
+            if (!vis[i]) {
+                dfs(adj, vis, path, i, safeNodes);
+            }
+        }
+        
+        vector<int> result;
+        for (int i = 0; i < V; ++i) {
+            if (safeNodes[i]) {
+                result.push_back(i);
+            }
+        }
+        
+        return result; 
     }
 };
