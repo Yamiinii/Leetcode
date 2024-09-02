9ce6a079f4d8ac6642d0e30b98f6774bf231ab87
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-09-02 18:36:13 +0530

    Time: 22 ms (26.84%), Space: 16.7 MB (23.77%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,34 +1,36 @@
+#include <vector>
+#include <queue>
+using namespace std;
+
 class Solution {
 public:
-    //linear graphs with no cycle are always biparatite
-    //colouring adj differently
-    //any graph even length cycle
     bool isBipartite(vector<vector<int>>& graph) {
-        vector<int> colors(graph.size(),-1);
-    for (int start = 0; start < graph.size(); ++start) {
-            if (colors[start] == -1) { // If this node hasn't been colored yet
+        int n = graph.size();
+        vector<int> color(n, -1); // Color array to track the color assigned to each node
+        
+        // Iterate over all nodes to handle disconnected components
+        for (int start = 0; start < n; ++start) {
+            if (color[start] == -1) { // If the node is not colored
                 queue<int> q;
                 q.push(start);
-                colors[start] = 0; // Start coloring with 0
+                color[start] = 0; // Start coloring the node with color 0
 
                 while (!q.empty()) {
                     int front = q.front();
                     q.pop();
 
-                    for (auto adj : graph[front]) {
-                        if (colors[adj] == -1) {
-                            // Color with the opposite color of the current node
-                            colors[adj] = !colors[front];
-                            q.push(adj);
-                        } else if (colors[adj] == colors[front]) {
-                            // If adjacent node has the same color, it's not bipartite
+                    for (auto neighbor : graph[front]) {
+                        if (color[neighbor] == -1) { // If the neighbor has not been colored
+                            color[neighbor] = !color[front]; // Color with opposite color
+                            q.push(neighbor);
+                        } else if (color[neighbor] == color[front]) { // Check for conflict
                             return false;
                         }
                     }
                 }
             }
         }
-
+        
         return true;
     }
 };
