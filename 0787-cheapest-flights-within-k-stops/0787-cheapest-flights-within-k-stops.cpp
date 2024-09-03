79423209ac9a835627a03f8ade20ba9c0583c9c0
Yamini <95957635+Yamiinii@users.noreply.github.com>
2024-09-03 21:59:34 +0530

    Time: 13 ms (80.53%), Space: 16.9 MB (74.04%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,48 +1,33 @@
-#include <vector>
-#include <queue>
-#include <unordered_map>
-#include <limits>
-
-using namespace std;
-
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int, int>>> graph(n);
-        for(auto e : flights) {
-            graph[e[0]].push_back({e[1], e[2]});
+              vector<vector<pair<int, int>>> adj(n);
+        for (const auto& flight : flights) {
+            int u = flight[0];
+            int v = flight[1];
+            int w = flight[2];
+            adj[u].emplace_back(v, w);
         }
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
-        // {dist_from_src_node, node, number_of_stops_from_src_node}
-        pq.push({0, src, 0});
-        
-        vector<int> stops(n, INT_MAX);
-        // number of stops to reach indexth node with least possible price from src node
-        // as it will be calculated once the pq.top() equals to indexth node
-        
-        // pq.top() will always store least cost among the pq elements so if already stop is calculated
-        // and if that is greater than the cstop that means already we have a path with cheaper cost
-        // as well as with less or equal number of stops
-        
-        while(!pq.empty()) {
-            auto temp=pq.top();
-            int cdist=temp[0];
-            int cnode=temp[1];
-            int cstop=temp[2];
-            pq.pop();
-            
-            if(cstop>stops[cnode] || cstop>k+1)
+           queue<tuple<int, int, int>> q;
+        q.push({0, src, 0});
+        vector<int> dist(n,INT_MAX);
+        dist[src] = 0;
+        while(!q.empty())
+        {
+            auto [stops,node,val]=q.front();
+            q.pop();
+            if(stops>k)
                 continue;
-            
-            stops[cnode]=cstop;
-            if(cnode==dst) {
-                return cdist;
-            }
-            
-            for(auto a : graph[cnode]) {
-                pq.push({cdist+a.second, a.first, cstop+1});
+            // if(node==dst)
+            //     return val;
+           for (const auto& [neighbor, weight] : adj[node]) {
+                if (val + weight < dist[neighbor] && stops <= k) {
+                    dist[neighbor] = val + weight;
+                    q.push({stops + 1, neighbor, dist[neighbor]});
+                }
             }
         }
-        return -1;
+        return dist[dst] == numeric_limits<int>::max() ? -1 : dist[dst];
+        
     }
 };
