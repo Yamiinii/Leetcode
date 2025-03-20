74a771436b77831711c410cdfcc126892f511d70
Yamini <95957635+Yamiinii@users.noreply.github.com>
2025-03-20 09:46:20 +0530

    Time: 2 ms (78.23%), Space: 15 MB (21.3%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,1 +1,18 @@
-class Solution { public: vector<int> twoSum(vector<int>& nums, int target) { vector<int> ans; unordered_map <int,int> mpp; for(int i=0;i<nums.size();i++) { if(mpp.find(target-nums[i])!=mpp.end()) { ans.push_back(mpp[target-nums[i]]); ans.push_back(i); return ans; } mpp[nums[i]]=i; } return ans; } };
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        unordered_map<int,int> mpp;
+        for(int i=0;i<n;i++)
+        {
+            int num1=nums[i];
+            int num2=target-num1;
+            if(mpp.find(num2)!=mpp.end())
+            {
+                return {i,mpp[num2]};
+            }
+            mpp[num1]=i;
+        }
+        return vector<int> {-1,-1};
+    }
+};
