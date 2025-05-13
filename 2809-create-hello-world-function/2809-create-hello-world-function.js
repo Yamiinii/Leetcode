cd2a58745fb4f623d443ba5fc526aa65e64c43b7
Yamini <95957635+Yamiinii@users.noreply.github.com>
2025-05-13 12:34:22 +0530

    Time: 30 ms (96.5%), Space: 52.6 MB (89.43%) - LeetHub

---
+++ 2809-create-hello-world-function/2809-create-hello-world-function.js
@@ -0,0 +1,27 @@
+/**
+ * @return {Function}
+ */
+// var createHelloWorld = function() {
+    
+//     return function(...args) {
+        
+//     }
+// };
+
+// var createHelloWorld = function(){
+
+//     return function(...args)
+//     {
+//         return "Hello World";
+//     }
+// };
+
+var createHelloWorld= function()
+{
+    return ()=>"Hello World";
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
