bfa5cc675e616fabbbaa93f8913d495730eed4d6
Yamini <95957635+Yamiinii@users.noreply.github.com>
2025-05-13 13:12:23 +0530

    Time: 34 ms (89.59%), Space: 53.4 MB (63.25%) - LeetHub

---
+++ 2732-counter/2732-counter.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+// var createCounter = function(n) {
+    
+//     return function() {
+        
+//     };
+// };
+
+// 1st method
+// var createCounter =function(n)
+// {
+//     return function()
+//     {
+//         return n++;
+//     }
+// }
+
+//2nd
+var createCounter=function(n)
+{
+    return ()=> n++;
+}
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
