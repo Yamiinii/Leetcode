49ad9dc3aab5b5a2d6ba3a9876ac1b3ff222086e
Yamini <95957635+Yamiinii@users.noreply.github.com>
2025-05-14 22:35:30 +0530

    Time: 52 ms (6.74%), Space: 53.3 MB (70.94%) - LeetHub

--- 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.js
+++ 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.js
@@ -2,24 +2,37 @@
  * @param {string} val
  * @return {Object}
  */
-var expect = function(val) {
-    function toBe(val1)
-    {
-        if(val===val1)
-        return true;
-        else
-        throw new Error( "Not Equal")
-    }
+// var expect = function(val) {
+//     function toBe(val1)
+//     {
+//         if(val===val1)
+//         return true;
+//         else
+//         throw new Error( "Not Equal")
+//     }
 
-       function notToBe(val1)
-    {
-        if(val!==val1)
-        return true;
-        else
-         throw new Error( "Equal")
-    }
-    return {toBe,notToBe};
+//        function notToBe(val1)
+//     {
+//         if(val!==val1)
+//         return true;
+//         else
+//          throw new Error( "Equal")
+//     }
+//     return {toBe,notToBe};
+// };
+var expect = function(val) {
+    return {
+        toBe: function(val1) {
+            if (val === val1) return true;
+            throw new Error("Not Equal");
+        },
+        notToBe: function(val1) {
+            if (val !== val1) return true;
+            throw new Error("Equal");
+        }
+    };
 };
+
 
 /**
  * expect(5).toBe(5); // true
