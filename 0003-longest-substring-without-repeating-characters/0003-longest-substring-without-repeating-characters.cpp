18cefd86610c400c5944d6f6760c8d345d41eeda
Yamini <95957635+Yamiinii@users.noreply.github.com>
2025-05-28 09:45:33 +0530

    Time: 11 ms (52.44%), Space: 11.8 MB (69.27%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,26 +1,40 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int left=0;
-        int right=0;
         int n=s.size();
-        int ans=0;
-        unordered_map<char,int>mpp;
-        while(right<n)
+        if(n==1)
+        return 1;
+        if(n==0)
+        return 0;
+        int front=0;
+        int back=0;
+        int maxi=1;
+        unordered_map<char,int> mpp;
+        mpp[s[0]]++;
+        //didn't check for back out of bounds here
+        //back<n is wrong but not back<n-1
+        //This means that on the final iteration where back == n - 1, you do back++, making back == n, and then access s[back], which is out-of-bounds.
+        while(front<=back && back<n-1)
         {
-            if(mpp.count(s[right]) == 0  ||mpp[s[right]]<left)
+            back++;
+            // I forgot to increment in the map
+            mpp[s[back]]++;
+            //did mpp[back] here by mistake also i wrote mpp[s[back]] > 0
+            if (mpp[s[back]] > 1)
             {
-                mpp[s[right]]=right;
-                ans=max(ans,right-left+1);
-                
+                // while(s[front]!=s[back])
+                while (mpp[s[back]] > 1)
+                {
+                    mpp[s[front]]--;
+                    front++;
+                }
+                // front++;
+                // maxi=max(maxi,back-front+1);
             }
-            else
-            {
-                left=mpp[s[right]]+1;
-                mpp[s[right]]=right;
-            }
-            right++;
+            //mpp[s[back]]++;
+            maxi=max(maxi,back-front+1);
         }
-        return ans;
+
+        return maxi;
     }
 };
