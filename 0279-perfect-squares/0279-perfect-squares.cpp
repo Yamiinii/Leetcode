a491472fbcc181a918c792c7e7e4fdc1912da998
Yamini <95957635+Yamiinii@users.noreply.github.com>
2025-05-29 13:09:59 +0530

    Time: 31 ms (92.69%), Space: 13 MB (82.88%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -17,6 +17,16 @@ public:
     int numSquares(int n) {
         vector<int> dp(n+1,-1);
         dp[0]=0;
-        return rec(n,dp);
+        for(int i=1;i<=n;i++)
+        {
+            dp[i]=i;
+            for(int j=1;j*j<=i;j++)
+            {
+                int sq=j*j;
+                dp[i]=min(dp[i],1+dp[i-sq]);
+            }
+        }
+
+        return dp[n];
     }
 };
