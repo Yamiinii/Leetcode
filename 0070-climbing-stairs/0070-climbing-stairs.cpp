ecf9b301b9539d09d6af46db295aaee238ca50ae
Yamini <95957635+Yamiinii@users.noreply.github.com>
2025-05-29 10:21:37 +0530

    Time: 0 ms (100%), Space: 8.7 MB (17.43%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        if(n <= 2) return n;
+    int rec(int n,vector<int> & dp)
+    {
+        if(n==1||n==2||n==0)
+        return dp[n]=n;
 
-        vector<int> dp(n + 1, 0);
-        dp[1] = 1;
-        dp[2] = 2;
-
-        for(int i = 3; i <= n; i++)
-            dp[i] = dp[i - 1] + dp[i - 2];
-
+        if(dp[n]!=-1)
         return dp[n];
+
+        return dp[n]=rec(n-1,dp)+rec(n-2,dp);
+        //once we come to these stairs only one way is there
+    }
+
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        // dp[0]=0;
+        // dp[1]=1;
+        // dp[2]=2;
+        //what if n=1 dp[2] won't exist
+        return rec(n,dp);
     }
 };
