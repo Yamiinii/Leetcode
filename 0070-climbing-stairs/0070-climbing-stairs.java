93cf8a6e79dc469b478314292d2ab781e9afb8c9
Yamini <95957635+Yamiinii@users.noreply.github.com>
2025-05-29 10:42:40 +0530

    Time: 1 ms (1.34%), Space: 40.1 MB (96.65%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,25 +1,19 @@
 class Solution {
-    public int rec(int n,List<Integer> dp)
-    {
-        if(dp.get(n)!=-1)
-        return dp.get(n);
-
-        int result=rec(n-1,dp)+rec(n-2,dp);
-        dp.set(n,result);
-        return result;
-
-    }
-
     public int climbStairs(int n) {
-        List<Integer> dp = new ArrayList<>(Collections.nCopies(n + 1, -1));
+        if(n==0||n==1||n==2)
+        return n;
 
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-        if (n == 2) return 2;
+        List<Integer> dp=new ArrayList<>(Collections.nCopies(n+1,-1));
+        dp.set(0,0);
+        dp.set(1,1);
+        dp.set(2,2);
 
-        dp.set(0, 0);
-        dp.set(1, 1);
-        dp.set(2, 2);
-        return rec(n,dp);
+        for(int i=3;i<=n;i++)
+        {
+            int val=dp.get(i-1)+dp.get(i-2);
+            dp.set(i,val);
+        }
+
+        return dp.get(n);
     }
 }
