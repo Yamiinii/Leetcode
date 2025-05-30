f395a83604d155803e67edb360008cc2e2d84f44
Yamini <95957635+Yamiinii@users.noreply.github.com>
2025-05-30 17:31:31 +0530

    Time: 2 ms (26.81%), Space: 11.1 MB (10.74%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
-    int ways(string s ,vector<int> &dp,int i)
+    int rec(string s,int i,vector<int> &dp)
     {
         if(i==s.size())
-            return 1;
-        if(s.size()==0)
-            return 0;
+        return 1;
+        //cannot decode 0
         if(s[i]=='0')
-            return 0;
+        return 0;
         if(dp[i]!=-1)
-            return dp[i];
-        //considering only a single char
-        int ans=ways(s,dp,i+1);
-        if(i<s.size()-1 && (s[i]=='1' || s[i]=='2' && s[i+1]<='6'))
-            ans+=ways(s,dp,i+2);
-        
-        return dp[i]=ans;
+        return dp[i];
+        int ways=0;
+        //for one char
+        ways+=rec(s,i+1,dp);
+        //I can use dp to calculate one and two char parts also 
+        //for two char
+        if(i+1<s.size() && (s[i]=='1' || (s[i]=='2' && s[i+1]>='0' && s[i+1]<='6')))
+        {
+            ways+=rec(s,i+2,dp);
+        }
+
+        return dp[i]=ways;
     }
-    
+
     int numDecodings(string s) {
         int n=s.size();
-        vector<int> dp(n,-1);
-        return ways(s,dp,0);
+        if(n==0)
+        return 0;
+        vector<int> dp(n+1,-1);
+        return rec(s,0,dp);
     }
 };
