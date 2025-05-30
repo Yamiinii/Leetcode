312484818262b42a6d3f9946d5d2b6bbe75d96ce
Yamini <95957635+Yamiinii@users.noreply.github.com>
2025-05-30 17:54:06 +0530

    Time: 0 ms (100%), Space: 8.7 MB (60.06%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
-    int rec(string s,int i,vector<int> &dp)
-    {
-        if(i==s.size())
-        return 1;
-        //cannot decode 0
-        if(s[i]=='0')
-        return 0;
-        if(dp[i]!=-1)
-        return dp[i];
-        int ways=0;
-        //for one char
-        ways+=rec(s,i+1,dp);
-        //I can use dp to calculate one and two char parts also 
-        //for two char
-        if(i+1<s.size() && (s[i]=='1' || (s[i]=='2' && s[i+1]>='0' && s[i+1]<='6')))
-        {
-            ways+=rec(s,i+2,dp);
-        }
-
-        return dp[i]=ways;
-    }
-
     int numDecodings(string s) {
         int n=s.size();
-        if(n==0)
-        return 0;
-        vector<int> dp(n+1,-1);
-        return rec(s,0,dp);
+        if (n == 0 || s[0] == '0') return 0;
+        vector<int> dp(n+1,0);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            if(s[i-1]!='0')
+            dp[i]+=dp[i-1];
+
+            //if (s[i-1]=='1' || (s[i-1]=='2' && s[i]>='0' && s[i]<='6'))
+            if (s[i - 2] == '1' || (s[i - 2] == '2' && s[i - 1] >= '0' && s[i - 1] <= '6'))
+            dp[i]+=dp[i-2];
+        }
+
+        return dp[n];
     }
 };
