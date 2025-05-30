7109e2dab094c4141bef816d304ad7a1fbaaf052
Yamini <95957635+Yamiinii@users.noreply.github.com>
2025-05-30 13:12:44 +0530

    Time: 0 ms (100%), Space: 10.8 MB (5.9%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
+    int rec(vector<int> & nums,vector<int> & dp,int i)
+    {
+        if(i>=nums.size())
+        return 0;
+
+        if(dp[i]!=-1)
+        return dp[i];
+
+        int ans=0;
+        int take=nums[i]+rec(nums,dp,i+2);
+        int not_take=rec(nums,dp,i+1);
+
+        ans=max(take,not_take);
+
+        return dp[i]=ans;
+    }
+
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 0) return 0; // If there are no houses, return 0
-        if (n == 1) return nums[0]; // If there is only one house, rob it
-        
-        vector<int> dp(n, 0);
-        
-        // Base cases
-        dp[0] = nums[0]; // Rob the first house
-        dp[1] = max(nums[0], nums[1]); // Rob either the first or the second house
-        
-        // Iterate over each house starting from the third one
-        for (int i = 2; i < n; i++) {
-            dp[i] = max(dp[i-1], dp[i-2] + nums[i]);
-        }
-        
-        // Return the maximum amount that can be robbed without triggering an alarm
-        return dp[n - 1];
+        int n=nums.size();
+        vector<int> dp(n+1,-1);
+        return rec(nums,dp,0);
     }
 };
