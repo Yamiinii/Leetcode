35b7dc644b96207aa37583c83481e12e5adda6d1
Yamini <95957635+Yamiinii@users.noreply.github.com>
2025-05-31 11:26:48 +0530

    Time: 0 ms (100%), Space: 13.5 MB (31.79%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
@@ -1,38 +1,34 @@
 class Solution {
 public:
-    int rec(vector<int>& days, vector<int>& costs,int i,vector<int> &dp)
-    {
-        if(i>=days.size())
-        return 0;
-
-        if(dp[i]!=-1)
-        return dp[i];
-
-        int cost=0;
-        //one day pass
-        int oneDay=costs[0]+rec(days,costs,i+1,dp);
-
-        //two days pass
-        int idx=i;
-        while(idx<days.size() && days[idx]<days[i]+7)
-        idx++;
-        int sevenDay=costs[1]+rec(days,costs,idx,dp);
-
-        //30 days pass
-        idx=i;
-        while(idx<days.size() && days[idx]<days[i]+30)
-        idx++;
-        int thirtyDay=costs[2]+rec(days,costs,idx,dp);
-
-        cost= std::min(oneDay, std::min(sevenDay, thirtyDay));
-
-        return dp[i]=cost;
-
-    }
-
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n=days.size();
         vector<int> dp(n+1,-1);
-        return rec(days,costs,0,dp);
+        dp[0]=0;
+        for(int i=1;i<=n;i++)
+        {
+            int day=days[i-1];
+            //one day 
+            int cost1=dp[i-1]+costs[0];
+
+            //two day
+            // After the loop:
+
+            // j is the last day not covered by the 7-day pass.
+
+            // So, j + 1 is the first day covered by this 7-day pass.
+            int j=i-1;
+            while(j>=0 && days[j]>=day-6)
+            j--;
+            int cost2=dp[j+1]+costs[1];
+
+            //thirty day
+            j=i-1;
+            while(j>=0 && days[j]>=day-29)
+            j--;
+            int cost3=dp[j+1]+costs[2];
+
+            dp[i]=min(cost1,min(cost2,cost3));
+        }
+        return dp[n];
     }
 };
