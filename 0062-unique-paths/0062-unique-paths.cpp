1728beaeedf559611b5cf284e422844d4e059c66
Yamini <95957635+Yamiinii@users.noreply.github.com>
2025-06-03 18:36:26 +0530

    Time: 0 ms (100%), Space: 9.4 MB (9.05%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-       int rec(int m, int n, int i, int j, std::vector<std::vector<int>>& dp) {
-     
-        if(i>=m ||j>=n)
+    int rec(int m,int n,vector<vector<int>> &dp,int i,int j)
+    {
+        if(i==m-1 || j==n-1)
+        return 1;
+
+        if (i >= m || j >= n)
             return 0;
-           
-        if (i == m - 1 && j == n - 1) {
-            return dp[i][j] = 1;
-        }
-     
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        
-        return dp[i][j] = rec(m,n,i+1,j,dp)+rec(m,n,i,j+1,dp);
+
+        if(dp[i][j]!=-1)
+        return dp[i][j];
+
+        int right=rec(m,n,dp,i+1,j);
+        int down=rec(m,n,dp,i,j+1);
+
+        return dp[i][j]=right+down;
     }
-    
+
     int uniquePaths(int m, int n) {
-       
-        std::vector<std::vector<int>> dp(m, std::vector<int>(n, -1));
- 
-        return rec(m, n, 0, 0, dp);
+        vector<vector<int>> dp(m+1,vector<int> (n+1,-1));
+        return rec(m,n,dp,0,0);
     }
 };
