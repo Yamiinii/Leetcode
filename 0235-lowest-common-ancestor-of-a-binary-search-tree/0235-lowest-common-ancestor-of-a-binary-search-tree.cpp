7f48ef4e3aa6688268892f1d18bf97495d60ca40
Yamini <95957635+Yamiinii@users.noreply.github.com>
2025-06-04 10:02:30 +0530

    Time: 28 ms (15.96%), Space: 23.5 MB (33.26%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,25 +10,24 @@
 
 class Solution {
 public:
+    TreeNode* rec(TreeNode* root, TreeNode* p, TreeNode* q)
+    {
+        if(root==NULL|| root==p || root==q)
+        return root;
+
+        TreeNode* left=rec(root->left,p,q);
+        TreeNode* right=rec(root->right,p,q);
+
+        if(left && right)
+        return root;
+
+        return left?left:right;
+    }
+
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (root == nullptr) {
-            return nullptr; // Base case: If the current node is null, return null.
-        }
-        
-        if (root == p || root == q) {
-            return root; // If either p or q is the current node, it's the common ancestor.
-        }
-        
-        // Recursively search in the left and right subtrees.
-        TreeNode* left = lowestCommonAncestor(root->left, p, q);
-        TreeNode* right = lowestCommonAncestor(root->right, p, q);
-        
-        if (left && right) {
-            return root; // If both left and right subtrees have found a common ancestor, root is the LCA.
-        } else if (left) {
-            return left; // If only left subtree has a common ancestor, return it.
-        } else {
-            return right; // If only right subtree has a common ancestor, return it.
-        }
+        if(root==p || root==q)
+        return root;
+
+        return rec(root,p,q);
     }
 };
