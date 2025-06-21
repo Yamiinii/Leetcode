2dd3b26a7a3397d448df6320301afa0375db72bf
Yamini <95957635+Yamiinii@users.noreply.github.com>
2025-06-21 10:46:08 +0530

    Time: 0 ms (100%), Space: 15.2 MB (32.91%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,34 +1,44 @@
 class Solution {
 public:
-    int search(vector<int>& arr, int key) {
-       int low=0;
-    int high=arr.size()-1;
-    while(low<=high)
+    int binarySearch(vector<int>& nums, int target)
     {
-        int mid=(low+high)/2;
+        int left=0;
+        int right=nums.size()-1;
 
-        if(arr[mid]==key)
-        return mid;
-
-        if(arr[low]<=arr[mid])
+        while(left<=right)
         {
-            if(key>=arr[low] && key<=arr[mid])
-            high=mid-1;
+//             | Formula                       | Safe? | Reason                         |
+// | ----------------------------- | ----- | ------------------------------ |
+// | `mid = (left + right)/2`      | ❌     | May cause integer overflow     |
+// | `mid = left + (right-left)/2` | ✅     | No overflow, standard practice |
+
+            int mid=left+(right-left)/2;
+
+            if(nums[mid]==target)
+            return mid;
+
+            //check if the left half is sorted or right half
+            if(nums[left]<=nums[mid])
+            {
+                if(nums[left]<=target && nums[mid]>target)
+                right=mid-1;
+                else
+                left=mid+1;
+            }
             else
-            low=mid+1;
+            {
+                if(nums[mid]<target && nums[right]>=target)
+                left=mid+1;
+                else
+                right=mid-1;
+            }
         }
 
-        else
-        {
-             if(key>=arr[mid] && key<=arr[high])
-            low=mid+1;
-            else
-            high=mid-1;
-        }
-        
+        return -1;
     }
 
-    return -1;
-        
+    int search(vector<int>& nums, int target) {
+        int pos=binarySearch(nums,target);
+        return pos;
     }
 };
