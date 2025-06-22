1b3ba106910335b93168968070fb9382180e55b4
Yamini <95957635+Yamiinii@users.noreply.github.com>
2025-06-22 10:54:20 +0530

    Time: 0 ms (100%), Space: 17.7 MB (56.4%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
-    //  ***********************KADANE********************************
-    //Ques : Why do we have a need to traverse from right to left ??
-// Ans : lets take an example
-// arr {-8,5,3,1,6}
-
-// // By traversing left to right we have an answer maxi1.
-// maxi1 = -720 , which is clearly not the max prod value.
-    
-    
     int maxProduct(vector<int>& nums) {
+        int n=nums.size();
+        int curr=1;
         int maxi=INT_MIN;
-        int product=1;
-        for(int i=0;i<nums.size();i++)
+        for(int i=0;i<n;i++)
         {
-            product*=nums[i];
-            maxi=max(product,maxi);
-            if(product==0)
-                product=1;
+            curr=curr*nums[i]; 
+
+            if(curr==0)
+            curr=nums[i];
+            
+            maxi=max(maxi,curr);
         }
-        
-        product=1;
-          for(int i=nums.size()-1;i>=0;i--)
+
+        curr=1;
+        for(int i=n-1;i>=0;i--)
         {
-            product*=nums[i];
-            maxi=max(product,maxi);
-            if(product==0)
-                product=1;
+            curr=curr*nums[i]; 
+
+            if(curr==0)
+            curr=nums[i];
+            
+            maxi=max(maxi,curr);
         }
-        
+
         return maxi;
     }
 };
