1c42155d9eb8903d7589ea901c61fd3f26e2dda1
Yamini <95957635+Yamiinii@users.noreply.github.com>
2025-06-24 22:47:48 +0530

    Time: 15 ms (40.65%), Space: 11.7 MB (26.94%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,17 +1,47 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        vector<int> map(128,0);
-        for(auto c: t) map[c]++;
-    int counter=t.size(), begin=0, end=0, d=INT_MAX, head=0;
-        while(end<s.size()){
-            if(map[s[end++]]-->0) counter--; //in t
-            while(counter==0){ //valid
-                if(end-begin<d)  d=end-(head=begin);
-                if(map[s[begin++]]++==0) counter++;  //make it invalid
-            }  
-        }
-        return d==INT_MAX? "":s.substr(head, d);
-    
+       unordered_map<char,int> mpp1;
+       int n=s.size();
+       int m=t.size();
+       for(int i=0;i<m;i++)
+       {
+        mpp1[t[i]]++;
+       }
+       int desired=mpp1.size();
+       int curr=0;
+       unordered_map<char,int> mpp2;
+       int left=0;
+       int right=0;
+       int mini=INT_MAX;
+       int start=0;
+       while(right<n)
+       {
+            char c=s[right];
+            mpp2[c]++;
+
+            if(mpp1.count(c) && mpp1[c]==mpp2[c])
+            curr++;
+
+            while(left<=right && curr==desired)
+            {
+                if(right-left+1<mini)
+                {
+                    mini=right-left+1;
+                    start=left;
+                }
+
+                int ch=s[left];
+                mpp2[ch]--;
+                if(mpp1.count(ch) && mpp2[ch]<mpp1[ch])
+                curr--;
+
+                left++;
+            }
+
+            right++;
+       }
+
+       return mini==INT_MAX?"":s.substr(start,mini);
     }
 };
