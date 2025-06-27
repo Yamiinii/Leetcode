3e674b539c28d0ebedbf67bbd5236adf84dc3df1
Yamini <95957635+Yamiinii@users.noreply.github.com>
2025-06-27 18:08:32 +0530

    Time: 11 ms (25.55%), Space: 17 MB (7.47%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
-    bool check(string &s, string &p, int sstart, int pstart)
-    {
-        int n=s.size();
-        int m=p.size();
-        if(pstart==m)
-        return sstart==n;
+    unordered_map<string, bool> memo;
 
-        bool first=(sstart < n &&
+    bool check(string &s, string &p, int sstart, int pstart) {
+        string key = to_string(sstart) + "," + to_string(pstart);
+        if (memo.count(key)) return memo[key];
+
+        int n = s.size();
+        int m = p.size();
+
+        if (pstart == m) return memo[key] = (sstart == n);
+
+        bool firstMatch = (sstart < n &&
                           (s[sstart] == p[pstart] || p[pstart] == '.'));
 
-        if(pstart+1<m && p[pstart+1]=='*')
-        {
-            //ignoring the letters before * and then seeing if after start character matches sstart character
-            return (check(s,p,sstart,pstart+2)) ||
-            (first && check(s,p,sstart+1,pstart));
+        if (pstart + 1 < m && p[pstart + 1] == '*') {
+            // 1. Skip "x*" pair
+            // 2. Use one "x" if it matches, and keep pattern at same spot
+            return memo[key] = check(s, p, sstart, pstart + 2) ||
+                               (firstMatch && check(s, p, sstart + 1, pstart));
+        } else {
+            return memo[key] = (firstMatch && check(s, p, sstart + 1, pstart + 1));
         }
-        else
-        return (first && check(s,p,sstart+1,pstart+1));
     }
+
     bool isMatch(string s, string p) {
-        //if more letters are there in p then it will return false
-        //if it is a char or . then simply match
-        //if it is * then we cannot see till then next matching letter
-        // for ex our s =abbbbb p=a*bb so we cannot just iterate to the last b according to our logic it will match the b on 2nd index and then simply return false
-        //so we will check every occurence of the char and try to match the string 
-        int n=s.size();
-        int m=p.size();
-        // if(m>n)
-        // return false;
-        int sstart=0;
-        int pstart=0;
-        return check(s,p,sstart,pstart);
-        
+        return check(s, p, 0, 0);
     }
 };
