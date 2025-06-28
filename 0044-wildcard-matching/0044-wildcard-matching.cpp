fabe0da76d75343e32b1a29bf17fe742ea5f3293
Yamini <95957635+Yamiinii@users.noreply.github.com>
2025-06-28 19:09:56 +0530

    Time: 37 ms (29.74%), Space: 14.8 MB (74.01%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,30 +1,62 @@
 class Solution {
 public:
+    // bool check(string s, string p,int sstart,int pstart,vector<vector<int>>& dp)
+    // {
+    //     int n=s.size();
+    //     int m=p.size();
+    //     //string key=to_string(sstart)+','+to_string(pstart);
+    //     if(sstart==n && pstart==m)
+    //     return true;
+    //     //if(sstart==n || pstart==m)
+    //     if(pstart==m)
+    //     return false;
+    // //  if (mpp.find(key) != mpp.end())
+    // // return mpp[key];
+    // if(dp[sstart][pstart]!=-1)
+    // return dp[sstart][pstart];
+
+    //     if (sstart == n) {
+    //         // Only valid if all remaining pattern characters are '*'
+    //         for (int i = pstart; i < m; ++i) {
+    //             if (p[i] != '*') return dp[sstart][pstart]=0;
+    //         }
+    //         return dp[sstart][pstart]=1;
+    //     }
+    
+    //     if(p[pstart]=='*')
+    //     return    dp[sstart][pstart]=check(s,p,sstart+1,pstart,dp) || check(s,p,sstart,pstart+1,dp);
+        
+    //     else if((s[sstart]==p[pstart] || p[pstart]=='?'))
+    //     return    dp[sstart][pstart]=check(s,p,sstart+1,pstart+1,dp);
+
+    //     return    dp[sstart][pstart]=0;
+              
+    // }
+
     bool isMatch(string s, string p) {
         int n=s.size();
         int m=p.size();
+        // if(p=="*")
+        // return true;
+        //unordered_map<string,bool> mpp;
         vector<vector<bool>> dp(n+1,vector<bool>(m+1,false));
-        dp[0][0]=true; //empty strings will match
-
+        dp[0][0]=true;
         //now I will see if we s string is empty but there is a * to be matched then it will like the previous one
-        for(int j=1;j<=m;j++)
+        for(int i=1;i<=m;i++)
         {
-            if(p[j-1]=='*')
-            dp[0][j]=dp[0][j-1];
+            if(p[i-1]=='*')
+            dp[0][i]=dp[0][i-1];
         }
-
-        //now for the complete string
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=m;j++)
             {
                 if(s[i-1]==p[j-1] || p[j-1]=='?')
                 dp[i][j]=dp[i-1][j-1];
-                
-                else if(p[j-1]=='*')
-                dp[i][j]=dp[i-1][j] || dp[i][j-1];
-                //first is matching empty string we didn't consider any new letter from j 
-                //second is matching complete till now that is s[i-1] keeping * active
+                else if(p[j - 1] == '*')
+                dp[i][j]=dp[i-1][j]||dp[i][j-1];
+                else
+                dp[i][j]=false;
             }
         }
 
