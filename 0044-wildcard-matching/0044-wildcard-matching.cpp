bc2b0325da999188cdc972b3d32d70371500dbe3
Yamini <95957635+Yamiinii@users.noreply.github.com>
2025-06-28 19:07:34 +0530

    Time: 29 ms (72.12%), Space: 14.9 MB (57.03%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
-    unordered_map<long long, int> memo;
-
-    long long hashKey(int i, int j) {
-        return ((long long)i << 32) | j;
-    }
-
-    bool check(string &s, string &p, int i, int j) {
-        int n = s.size(), m = p.size();
-        if (i == n && j == m) return true;
-        if (j == m) return false;
-
-        long long key = hashKey(i, j);
-        if (memo.count(key)) return memo[key];
-
-        if (i == n) {
-            for (int k = j; k < m; k++) {
-                if (p[k] != '*') return memo[key] = 0;
-            }
-            return memo[key] = 1;
-        }
-
-        if (p[j] == '*') {
-            return memo[key] = check(s, p, i + 1, j) || check(s, p, i, j + 1);
-        }
-
-        if (s[i] == p[j] || p[j] == '?') {
-            return memo[key] = check(s, p, i + 1, j + 1);
-        }
-
-        return memo[key] = 0;
-    }
-
     bool isMatch(string s, string p) {
-        return check(s, p, 0, 0);
+        int n=s.size();
+        int m=p.size();
+        vector<vector<bool>> dp(n+1,vector<bool>(m+1,false));
+        dp[0][0]=true; //empty strings will match
+
+        //now I will see if we s string is empty but there is a * to be matched then it will like the previous one
+        for(int j=1;j<=m;j++)
+        {
+            if(p[j-1]=='*')
+            dp[0][j]=dp[0][j-1];
+        }
+
+        //now for the complete string
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s[i-1]==p[j-1] || p[j-1]=='?')
+                dp[i][j]=dp[i-1][j-1];
+                
+                else if(p[j-1]=='*')
+                dp[i][j]=dp[i-1][j] || dp[i][j-1];
+                //first is matching empty string we didn't consider any new letter from j 
+                //second is matching complete till now that is s[i-1] keeping * active
+            }
+        }
+
+        return dp[n][m];
     }
 };
