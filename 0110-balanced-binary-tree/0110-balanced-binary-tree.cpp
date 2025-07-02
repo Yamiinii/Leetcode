82904670224b154f6bdf2284cca31420ad629b53
Yamini <95957635+Yamiinii@users.noreply.github.com>
2025-07-02 08:10:04 +0530

    Time: 0 ms (100%), Space: 22.9 MB (97.56%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -1,27 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int maxDepth(TreeNode* node) {
-        if (node == nullptr)
-            return 0;
-        else {
-            int lDepth = maxDepth(node->left);
-            int rDepth = maxDepth(node->right);
-            return 1 + max(lDepth, rDepth);  // Use the larger depth
-        }
+    // int heigh(TreeNode* root)
+    // {
+    //     if(root==NULL)
+    //     return 0;
+
+    //     int lheight=heigh(root->left);
+    //     int rheight=heigh(root->right);
+
+    //     return 1+max(lheight,rheight);
+
+    // }
+
+    // bool rec(TreeNode* root)
+    // {
+    //     if(root==NULL)
+    //     return 0;
+
+    //     bool left=rec(root->left);
+    //     bool right=rec(root->right);
+
+    //     int lheight=heigh(root->left);
+    //     int rheight=heigh(root->right);
+
+    //     return left && right && |lheight-rheight|<=1;
+    // }
+
+        pair<bool, int> rec(TreeNode* root) {
+        if (root == nullptr)
+            return {true, 0};
+
+        pair<bool, int> left = rec(root->left);
+        pair<bool, int> right = rec(root->right);
+
+        bool balanced = left.first && right.first && abs(left.second - right.second) <= 1;
+        int height = 1 + max(left.second, right.second);
+
+        return {balanced, height};
     }
 
     bool isBalanced(TreeNode* root) {
-        if (root == nullptr) {
-            return true;  // An empty tree is considered balanced
-        }
-
-        int leftDepth = maxDepth(root->left);
-        int rightDepth = maxDepth(root->right);
-
-        if (abs(leftDepth - rightDepth) <= 1 && isBalanced(root->left) && isBalanced(root->right)) {
-            return true;
-        }
-
-        return false;
+        return rec(root).first;
     }
 };
